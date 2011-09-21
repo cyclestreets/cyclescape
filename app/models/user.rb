011f8ecb93a21f94117113790cc362168b237177
Andrew France <andrew@avito.co.uk>
2011-09-21 15:38:11 +0100

    Activate Devise for user model and controllers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable
+
   has_many :memberships, class_name: "GroupMembership"
   has_many :groups, through: :memberships
   has_many :issues, foreign_key: "created_by_id"
