c479edeb62a514c8a1c697937c91bb015e6c7a64
Andrew France <andrew@avito.co.uk>
2011-09-21 17:38:12 +0100

    Make groups have nice URLs.

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ActiveRecord::Base
   has_many :memberships, class_name: "GroupMembership"
   has_many :members, through: :memberships
   has_many :threads, class_name: "MessageThread"
+
+  def to_param
+    "#{id}-#{short_name}"
+  end
 end
