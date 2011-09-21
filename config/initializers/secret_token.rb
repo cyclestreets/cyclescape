e0bf8bf24dc2f4441a2b76221db4a69e531d0d8f
Andrew France <andrew@avito.co.uk>
2011-09-21 10:37:30 +0100

    Initial Rails 3.1 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyclekit::Application.config.secret_token = 'a1bcbfb276fb310924d6c5f8c7ca23d880200baf4bfe99a3769e2e889eedf6dd941d293f4d82fd38487da6fedbb71076d01ccf6f630ae64326a84d32ba2526ce'
