4d5f203ac10949a2bb5ba3458f8dbff0a04d3852
Andy Allan <andy@gravitystorm.co.uk>
2011-09-21 14:30:22 +0100

    Add the GroupMembership model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110921130105) do
+ActiveRecord::Schema.define(:version => 20110921132554) do
+
+  create_table "group_memberships", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.integer  "group_id",   :null => false
+    t.string   "role",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.datetime "deleted_at"
+  end
 
   create_table "groups", :force => true do |t|
     t.string   "name",        :null => false
