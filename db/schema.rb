79bb6ed1905efa9500d54719c2ccb05598c1c437
Andrew France <andrew@avito.co.uk>
2011-09-21 12:41:46 +0100

    Create Group model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110921105617) do
+ActiveRecord::Schema.define(:version => 20110921113111) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name",        :null => false
+    t.string   "short_name",  :null => false
+    t.string   "website"
+    t.string   "email"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.datetime "disabled_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
