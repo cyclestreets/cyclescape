232814cf18e44d0153154c254f87c2959b8995cf
Andy Allan <andy@gravitystorm.co.uk>
2011-09-22 16:13:00 +0100

    Make sure categories have unique names, and limit their length too

--- app/models/issue_category.rb
+++ app/models/issue_category.rb
@@ -1,5 +1,5 @@
 class IssueCategory < ActiveRecord::Base
   has_many :issues, foreign_key: "category_id"
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 60 }
 end
--- spec/models/issue_category_spec.rb
+++ spec/models/issue_category_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe IssueCategory do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Newly created" do
+    subject { FactoryGirl.create(:issue_category) }
+
+    it "must have a name" do
+      subject.name.should be_an_instance_of(String)
+    end
+  end
+
+  describe "to be valid" do
+    subject { FactoryGirl.create(:issue_category) }
+
+    it "must have a name" do
+      subject.name = nil
+      subject.should_not be_valid
+    end
+
+    it "must not have a stupidly long name" do
+      subject.name = "F"*100
+      subject.should_not be_valid
+    end
+
+    it { should validate_uniqueness_of(:name) }
+  end
 end
