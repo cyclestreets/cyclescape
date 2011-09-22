a1ab71a18ff0932103698d39f8a92d4772e4e929
Andy Allan <andy@gravitystorm.co.uk>
2011-09-22 15:58:43 +0100

    Add a category to the issue factory, since it's now required

---
+++ spec/factories/issue_categories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :issue_category do
+    name "Bike Parking"
+  end
+end
--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     title "There's something wrong"
     description "Whose leg do you have to hump to get a dry martini around here?"
     association :created_by, factory: :user
+    association :category, factory: :issue_category
   end
 end
