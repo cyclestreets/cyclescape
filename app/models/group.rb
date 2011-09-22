a348de1a9ce6b00226d2c55838764430bbc5689f
Andrew France <andrew@avito.co.uk>
2011-09-22 17:08:22 +0100

    Add Group factory and validations with tests.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,9 @@
 class Group < ActiveRecord::Base
   has_many :memberships, class_name: "GroupMembership"
-  has_many :members, through: :memberships
+  has_many :members, through: :memberships, source: :user
   has_many :threads, class_name: "MessageThread"
+
+  validates :name, :short_name, presence: true
 
   def to_param
     "#{id}-#{short_name}"
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "to be valid" do
+    subject { Factory.build(:group) }
+
+    it "must have a name" do
+      subject.name = ""
+      subject.should have(1).error_on(:name)
+    end
+
+    it "must have a short name" do
+      subject.short_name = ""
+      subject.should have(1).error_on(:short_name)
+    end
+  end
 end
