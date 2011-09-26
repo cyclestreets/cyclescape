6681210e629cabb46ec2dd10d055bf350d356066
Andy Allan <andy@gravitystorm.co.uk>
2011-09-26 15:27:40 +0100

    First steps at displaying lines and polygons.
    Rather than implementing them manually by pulling out coordinates and constructing features, will be better to use a geojson layer instead.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -9,4 +9,15 @@ class Issue < ActiveRecord::Base
   validates :category, presence: true
 
   self.rgeo_factory_generator = RGeo::Geos.factory_generator
+
+  # Define an approximate centre of the issue, for convenience.
+  # Note that the line or polygon might be nowhere near this centre
+  def centre
+    case self.location.geometry_type
+    when RGeo::Feature::Point
+      return self.location
+    else
+      return self.location.envelope.centroid
+    end
+  end
 end
