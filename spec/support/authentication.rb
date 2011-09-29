6b27891df7ab68de710e4d62912ffade774ed439
Andrew France <andrew@avito.co.uk>
2011-09-29 16:52:08 +0100

    Tidy up and add guard to authentication support.

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,18 +1,19 @@
 shared_context "signed in as admin" do
-  let(:admin) { FactoryGirl.create(:stewie) }
-  let(:password) { FactoryGirl.attributes_for(:stewie)[:password] }
+  let!(:admin) { FactoryGirl.create(:stewie) }
+  let!(:password) { FactoryGirl.attributes_for(:stewie)[:password] }
 
   before do
     visit new_user_session_path
     fill_in "Email", with: admin.email
     fill_in "Password", with: password
     click_button "Sign in"
+    page.should have_content("Signed in")
   end
 end
 
 shared_context "signed in as a committee member" do
-  let(:membership) { FactoryGirl.create(:brian_at_quahogcc) }
-  let(:password) { FactoryGirl.attributes_for(:brian_at_quahogcc)[:password] }
+  let!(:membership) { FactoryGirl.create(:brian_at_quahogcc) }
+  let!(:password) { FactoryGirl.attributes_for(:brian)[:password] }
   let(:current_group) { membership.group }
   let(:current_user) { membership.user }
 
@@ -21,5 +22,6 @@ shared_context "signed in as a committee member" do
     fill_in "Email", with: current_user.email
     fill_in "Password", with: password
     click_button "Sign in"
+    page.should have_content("Signed in")
   end
 end
