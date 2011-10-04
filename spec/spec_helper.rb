2a93a0cf171d78e493f021dafc94c02641c05616
Andrew France <andrew@avito.co.uk>
2011-10-04 16:21:12 +0100

    Reload translations in Spork.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,6 +61,9 @@ Spork.each_run do
   # This code will be run each time you run your specs.
   FactoryGirl.reload
 
+  # Reload translations
+  I18n.reload!
+
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
