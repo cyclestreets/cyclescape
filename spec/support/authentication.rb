9f893165d08359e47d7c461c66862d765ea1eada
Andrew France <andrew@avito.co.uk>
2011-10-05 12:49:10 +0100

    Refactor authentication rspec support file and add site user.

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,22 +1,4 @@
-shared_context "signed in as admin" do
-  let!(:admin) { FactoryGirl.create(:stewie) }
-  let!(:password) { FactoryGirl.attributes_for(:stewie)[:password] }
-
-  before do
-    visit new_user_session_path
-    fill_in "Email", with: admin.email
-    fill_in "Password", with: password
-    click_button "Sign in"
-    page.should have_content("Signed in")
-  end
-end
-
-shared_context "signed in as a committee member" do
-  let!(:membership) { FactoryGirl.create(:brian_at_quahogcc) }
-  let!(:password) { FactoryGirl.attributes_for(:brian)[:password] }
-  let(:current_group) { membership.group }
-  let(:current_user) { membership.user }
-
+shared_context "signs in" do
   before do
     visit new_user_session_path
     fill_in "Email", with: current_user.email
@@ -24,4 +6,27 @@ shared_context "signed in as a committee member" do
     click_button "Sign in"
     page.should have_content("Signed in")
   end
+end
+
+shared_context "signed in as admin" do
+  include_context "signs in"
+
+  let!(:current_user) { FactoryGirl.create(:stewie) }
+  let!(:password) { FactoryGirl.attributes_for(:stewie)[:password] }
+end
+
+shared_context "signed in as a committee member" do
+  include_context "signs in"
+
+  let!(:membership) { FactoryGirl.create(:brian_at_quahogcc) }
+  let!(:password) { FactoryGirl.attributes_for(:brian)[:password] }
+  let(:current_group) { membership.group }
+  let(:current_user) { membership.user }
+end
+
+shared_context "signed in as a site user" do
+  include_context "signs in"
+
+  let!(:current_user) { FactoryGirl.create(:user) }
+  let!(:password) { FactoryGirl.attributes_for(:user)[:password] }
 end
