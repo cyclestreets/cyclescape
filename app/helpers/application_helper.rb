46a6d4eb86a2f524753d39dda5b7b4f892498a2d
Andrew France <andrew@avito.co.uk>
2011-10-07 12:34:35 +0100

    Update issue show page to add links to create messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
       link_to t("cancel"), url
     end
   end
+
+  def user_groups(user = nil)
+    user ||= current_user
+    return [] if user.nil?
+    user.groups
+  end
 end
