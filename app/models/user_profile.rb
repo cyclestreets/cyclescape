b62f5b45c30346297faec0140711a83d06db1eae
Andrew France <andrew@avito.co.uk>
2011-10-12 16:13:10 +0100

    User profile picture upload and display.

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,5 +1,14 @@
 class UserProfile < ActiveRecord::Base
-  image_accessor :picture
+  image_accessor :picture do
+    storage_path :generate_picture_path
+  end
 
   belongs_to :user
+
+  protected
+
+  def generate_picture_path
+    hash = Digest::SHA1.file(picture.path).hexdigest
+    "attachments/user_profile/#{hash[0..6]}/#{hash}"
+  end
 end
