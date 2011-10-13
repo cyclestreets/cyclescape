4d9f67281cb57b73f0b6324175055600b0b267f4
Andrew France <andrew@avito.co.uk>
2011-10-13 23:25:03 +0100

    Set photo storage path.

--- app/models/messages/photo_message.rb
+++ app/models/messages/photo_message.rb
@@ -1,7 +1,16 @@
 class PhotoMessage < MessageComponent
   # Core associations defined in MessageComponent
 
-  image_accessor :photo
+  image_accessor :photo do
+    storage_path :generate_photo_path
+  end
 
   validates :photo, presence: true
+
+  protected
+
+  def generate_photo_path
+    hash = Digest::SHA1.file(photo.path).hexdigest
+    "message_photos/#{hash[0..2]}/#{hash[3..5]}/#{hash}"
+  end
 end
