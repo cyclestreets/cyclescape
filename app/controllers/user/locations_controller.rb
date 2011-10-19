df8a49735fea2192b8d98aac2df453769b00d1fc
Andy Allan <andy@gravitystorm.co.uk>
2011-10-19 11:55:44 +0100

    create user locations

--- app/controllers/user/locations_controller.rb
+++ app/controllers/user/locations_controller.rb
@@ -4,5 +4,17 @@ class User::LocationsController < ApplicationController
 
   def new
     @location = current_user.locations.new
+    @start_location = RGeo::Geos::Factory.create({has_z_coordinate: true}).point(0.1477639423685, 52.27332049515, 14)
+  end
+
+  def create
+    @location = current_user.locations.new(params[:user_location])
+
+    if @location.save
+      redirect_to action: :index
+    else
+      @start_location = RGeo::Geos::Factory.create({has_z_coordinate: true}).point(0.1477639423685, 52.27332049515, 14)
+      render :new
+    end
   end
 end
