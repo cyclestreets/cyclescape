c759b2bfbecd6e7fae81f3dd2dbcda981f5100bc
Andy Allan <andy@gravitystorm.co.uk>
2011-10-19 14:32:34 +0100

    Add tests for user locations

--- spec/models/user_location_spec.rb
+++ spec/models/user_location_spec.rb
@@ -1,5 +1,60 @@
 require 'spec_helper'
 
 describe UserLocation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "newly created" do
+    subject { FactoryGirl.create(:user_location) }
+
+    it "must have a user" do
+      subject.user.should be_valid
+    end
+
+    it "must have a category" do
+      subject.category.should be_valid
+      subject.category.name.should be_a(String)
+    end
+
+    it "should have a geojson string" do
+      subject.loc_json.should be_a(String)
+      subject.loc_json.should eql(RGeo::GeoJSON.encode(subject.location).to_json)
+    end
+  end
+
+  describe "to be valid" do
+    subject { FactoryGirl.build(:user_location) }
+
+    it "must have a user" do
+      subject.user = nil
+      subject.should_not be_valid
+    end
+
+    it "must have a category" do
+      subject.category = nil
+      subject.should_not be_valid
+    end
+
+    it "must have a location" do
+      subject.location = nil
+      subject.should_not be_valid
+    end
+
+    it "must return an empty geojson string when no location" do
+      subject.location = nil
+      subject.loc_json.should be_a(String)
+      subject.loc_json.should eq("")
+    end
+
+    it "should accept a valid geojson string" do
+      subject.location = nil
+      subject.should_not be_valid
+      subject.loc_json = '{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[0.14,52.27]}}'
+      subject.should be_valid
+      subject.location.x.should eql(0.14)
+      subject.location.y.should eql(52.27)
+    end
+
+    it "should ignore a bogus geojson string" do
+      subject.loc_json = 'Garbage'
+      subject.should be_valid
+    end
+  end
 end
