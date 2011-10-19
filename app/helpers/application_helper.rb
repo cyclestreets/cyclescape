4bbc962dfb2c0443cdac02c9f09efe3e43fc2ca8
Andy Allan <andy@gravitystorm.co.uk>
2011-10-19 15:58:05 +0100

    Tiny display maps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,6 +21,41 @@ module ApplicationHelper
     end
   end
 
+  def tiny_display_map(object, &block)
+    zoom = 16
+    html_id = "tinymap_#{object.id}"
+    @map = MapLayers::Map.new(html_id, {theme: "/openlayers/theme/default/style.css", projection: googleproj, displayProjection: projection, controls: []}) do |map, page|
+      page << map.add_layer(OpenLayers::Layer::OSM.new("OpenCycleMap", ["a", "b", "c"].map {|k| "http://#{k}.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"}))
+
+      format = MapLayers::JsVar.new("format")
+      page.assign(format, OpenLayers::Format::GeoJSON.new(internalProjection: googleproj, externalProjection: projection))
+
+      format_plain = MapLayers::JsVar.new("format_plain")
+      page.assign(format_plain, OpenLayers::Format::GeoJSON.new)
+
+      if object.location.geometry_type == RGeo::Feature::Point
+        page << map.setCenter(OpenLayers::LonLat.new(object.location.x,object.location.y).transform(projection, map.getProjectionObject()),zoom);
+
+        markerlayer = MapLayers::JsVar.new('markerlayer')
+        icon = MapLayers::JsVar.new('icon')
+        page.assign(markerlayer, OpenLayers::Layer::Markers.new( "Location", { projection: projection }))
+        page << map.addLayer(markerlayer)
+        page.assign(icon, OpenLayers::Icon.new('/openlayers/img/marker-blue.png'))
+        page << markerlayer.addMarker(OpenLayers::Marker.new(OpenLayers::LonLat.new(object.location.x, object.location.y).transform(projection, map.getProjectionObject()), icon))
+      else
+        bbox = RGeo::Cartesian::BoundingBox.new(object.location.factory)
+        bbox.add(object.location)
+        page << map.zoomToExtent(OpenLayers::Bounds.new(bbox.min_x, bbox.min_y, bbox.max_x, bbox.max_y).transform(projection, map.getProjectionObject()))
+
+        locationlayer = MapLayers::JsVar.new('locationlayer')
+        protocol = OpenLayers::Protocol::HTTP.new( url: geometry_url(object), format: :format_plain )
+        page.assign(locationlayer, OpenLayers::Layer::Vector.new( "Location", protocol: protocol, projection: projection, strategies: [OpenLayers::Strategy::Fixed.new() ]))
+        page << map.addLayer(locationlayer)
+      end
+      yield(map, page, html_id) if block_given?
+    end
+  end
+
   def projection
     OpenLayers::Projection.new("EPSG:4326")
   end
@@ -33,5 +68,18 @@ module ApplicationHelper
     user ||= current_user
     return [] if user.nil?
     user.groups
+  end
+
+  private
+
+  def geometry_url(object)
+    # I suspect there's some metaprogramming possible here
+    #
+    case object.class.name.underscore
+    when "user_location"
+      geometry_user_location_path(object, :json)
+    when "issue"
+      geometry_issue_path(object, :json)
+    end
   end
 end
