e73a753e799542b2762cb674c44ef809731947e9
Andy Allan <andy@gravitystorm.co.uk>
2011-10-20 12:20:44 +0100

    Show recent issues in the groups area

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-    @recent_threads = @group.threads.limit(10)
+    @recent_threads = @group.threads.order("created_at DESC").limit(10)
+    @recent_issues = Issue.where("st_intersects(location, ?)", @group.profile.location).order("created_at DESC").limit(10)
   end
 end
