32a9614df4c0e5966104047c9e5d48b7488f5a63
Andy Allan <andy@gravitystorm.co.uk>
2011-10-20 17:35:28 +0100

    Move the url and api key to an initializer

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,3 @@
+GEOCODER_API_KEY = "b7af2f6899b5d784"
+GEOCODER_ENDPOINT = "http://www.cyclestreets.net/api/geocoder.json"
+GEOCODER_URL = GEOCODER_ENDPOINT + "?key=" + GEOCODER_API_KEY + "&callback=?"
