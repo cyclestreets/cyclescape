ed23141c435cea991e94811c822f9cd56fa6662e
Andy Allan <andy@gravitystorm.co.uk>
2011-10-26 12:27:39 +0100

    Use a recent_issues method on the group model, and take advantage of the new intersects mixin

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,6 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @recent_threads = @group.threads.order("created_at DESC").limit(10)
-    @recent_issues = Issue.where("st_intersects(location, ?)", @group.profile.location).order("created_at DESC").limit(10)
+    @recent_issues = @group.recent_issues.limit(10)
   end
 end
