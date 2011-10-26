1798a75236d6968df52e8b0ddb1b843f45e61ccd
Andrew France <andrew@avito.co.uk>
2011-10-27 00:10:28 +0100

    Refactoring of map search.
    * Note dependency on global map var that should be removed.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,4 @@
-GEOCODER_API_KEY = "b7af2f6899b5d784"
-GEOCODER_ENDPOINT = "http://www.cyclestreets.net/api/geocoder.json"
-GEOCODER_URL = GEOCODER_ENDPOINT + "?key=" + GEOCODER_API_KEY + "&callback=?"
+module Geocoder
+  API_KEY = "b7af2f6899b5d784"
+  URL = "http://www.cyclestreets.net/api/geocoder.json"
+end
