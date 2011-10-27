614af64f3e61d0d561767fa1509cd4b92f7a8cda
Andy Allan <andy@gravitystorm.co.uk>
2011-10-27 11:21:13 +0100

    Allow admins to edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,19 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.order("created_at DESC")
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:user])
+      flash.notice = t(".user_updated")
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
 end
