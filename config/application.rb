fc993ad16b125899408c929b91c75cf39c7044fc
Andrew France <andrew@avito.co.uk>
2011-10-28 17:53:28 +0100

    Set app name and title strings to Cyclescape, add logo placeholder.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-module Cyclekit
+module Cyclescape
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
