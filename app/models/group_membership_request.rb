0b38b9a5502679f375ea0d65926916c39f9fad11
Andy Allan <andy@gravitystorm.co.uk>
2011-10-28 14:03:35 +0100

    Convert to state machine

--- app/models/group_membership_request.rb
+++ app/models/group_membership_request.rb
@@ -1,11 +1,34 @@
 class GroupMembershipRequest < ActiveRecord::Base
-  ALLOWED_STATUS = %w(pending confirmed rejected cancelled)
+  attr_protected :state_event
 
   belongs_to :group
   belongs_to :user
   belongs_to :actioned_by, class_name: "User"
 
-  validates_associated :user
-  validates_associated :group
-  validates :status, inclusion: {in: ALLOWED_STATUS}
+  validates :user, presence: true
+  validates :group, presence: true
+
+  state_machine :status, :initial => :pending do
+    state :pending, :cancelled
+
+    state :confirmed, :rejected do
+      validates :actioned_by, presence: true
+    end
+
+    event :confirm do
+      transition :pending => :confirmed
+    end
+
+    event :reject do
+      transition :pending => :rejected
+    end
+
+    event :cancel do
+      transition :pending => :cancelled
+    end
+  end
+
+  def initalize
+    super
+  end
 end
