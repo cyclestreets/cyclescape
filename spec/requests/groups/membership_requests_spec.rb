8d1bb4afd8921435eaf9b40c99ef4916b683abbf
Andy Allan <andy@gravitystorm.co.uk>
2011-11-01 11:52:04 +0000

    Build the membership requests to explicitly use the group for the signed in user

--- spec/requests/groups/membership_requests_spec.rb
+++ spec/requests/groups/membership_requests_spec.rb
@@ -1,37 +1,30 @@
 require 'spec_helper'
 
 describe "Group Membership Requests" do
-  let(:request) { FactoryGirl.create(:meg_joining_quahogcc) }
+  let(:meg) { FactoryGirl.create(:meg) }
 
-  # The first one fails, but the others pass. Hmmm.
   context "as a group member" do
     include_context "signed in as a group member"
-
-      before do
-        visit group_membership_requests_path(request.group)
-      end
+    let(:gmr) { GroupMembershipRequest.create({group: current_group, user: meg}) }
 
     describe "viewing the requests" do
       it "should refuse" do
-        visit group_membership_requests_path(request.group)
+        visit group_membership_requests_path(gmr.group)
         page.should have_content("You are not authorised to access that page.")
       end
     end
   end
 
-  # This fails, and I don't understand why. If you actually try it yourself, it works.
   context "as a committee member" do
     include_context "signed in as a committee member"
-
-    before do
-      visit group_membership_requests_path(request.group)
-    end
+    let(:gmr) { GroupMembershipRequest.create({group: current_group, user: meg}) }
 
     describe "confirming a request" do
       it "should send a notification" do
+        visit group_membership_requests_path(gmr.group)
         click_on "Confirm"
-        open_email(request.user.email)
-        current_email.should have_subject("You are now a member of #{request.group.name}")
+        open_email(gmr.user.email)
+        current_email.should have_subject("You are now a member of #{gmr.group.name}")
       end
     end
   end
