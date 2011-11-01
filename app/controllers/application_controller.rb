91f4dff1439f7857c76fcd276ea100fc6c78e905
Andy Allan <andy@gravitystorm.co.uk>
2011-11-01 18:05:12 +0000

    Don't add a flash alert when directly rendering a 401 response.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,8 +28,7 @@ class ApplicationController < ActionController::Base
       flash.alert = t(".application.permission_denied_sign_in")
       redirect_to new_user_session_path
     else
-      flash.alert = t(".application.permission_denied")
-      render status: :unauthorized, text: "You are not authorised to access that page."
+      render status: :unauthorized, text: t(".application.permission_denied")
     end
   end
 
