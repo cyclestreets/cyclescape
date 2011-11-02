982afc1b26c5bd917521be0f3c0f6b8e58da7984
Andy Allan <andy@gravitystorm.co.uk>
2011-11-02 16:56:45 +0000

    Basic tests for deadlines

--- spec/models/messages/deadline_message_spec.rb
+++ spec/models/messages/deadline_message_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe Messages::DeadlineMessage do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe DeadlineMessage do
+  describe "associations" do
+    it { should belong_to(:thread) }
+    it { should belong_to(:message) }
+    it { should belong_to(:created_by) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:deadline) }
+    it { should validate_presence_of(:title) }
+  end
 end
