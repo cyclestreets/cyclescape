bb7514805876d6326e02ef8143ba1b2c4d4b0840
Andrew France <andrew@avito.co.uk>
2011-11-04 11:50:25 +0000

    Use real method to find nearby issues.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,6 @@ class DashboardsController < ApplicationController
   def show
     @user = current_user
     @groups = @user.groups
-    @relevant_issues = Issue.all(limit: 2)
+    @relevant_issues = current_user.issues_near_locations
   end
 end
