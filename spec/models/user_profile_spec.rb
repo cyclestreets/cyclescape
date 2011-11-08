062931435189eab42ad89bb69f4ccce30974f344
Andy Allan <andy@gravitystorm.co.uk>
2011-11-08 10:09:24 +0000

    Tweak the URL normalizer, along with the blank url test that triggered the bug

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -36,4 +36,44 @@ describe UserProfile do
       subject.should have(1).error_on(:picture)
     end
   end
+
+  context "url" do
+    subject { FactoryGirl.create(:user_profile) }
+
+    it "should allow a valid URL with HTTP protocol" do
+      subject.website = "http://en.wikipedia.org/wiki/Family_Guy"
+      subject.should have(0).errors_on(:website)
+    end
+
+    it "should allow a valid URL with HTTPS protocol" do
+      subject.website = "https://en.wikipedia.org/wiki/Family_Guy"
+      subject.should have(0).errors_on(:website)
+    end
+
+    it "should allow a valid URL without protocol" do
+      subject.website = "en.wikipedia.org/wiki/Family_Guy"
+      subject.should have(0).errors_on(:website)
+    end
+
+    it "should prefix the HTTP protocol on a URL without protocol" do
+      subject.website = "en.wikipedia.org/wiki/Family_Guy"
+      subject.website.should == "http://en.wikipedia.org/wiki/Family_Guy"
+    end
+
+    it "should not allow a URL with FTP protocol" do
+      subject.website = "ftp://en.wikipedia.org/wiki/Family_Guy"
+      subject.should have(1).error_on(:website)
+    end
+
+    it "should not allow an invalid URL" do
+      subject.website = "w[iki]pedia.org/wiki/Family_Guy"
+      subject.should have(1).error_on(:website)
+    end
+
+    it "should accept a blank url" do
+      subject.website = ""
+      subject.website.should === ""
+      subject.should have(0).errors_on(:website)
+    end
+  end
 end
