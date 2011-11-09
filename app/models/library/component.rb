5729b32ddfb884d0294b6026dc3e5a0f47b324fc
Andrew France <andrew@avito.co.uk>
2011-11-09 17:52:42 +0000

    Refactor Library Document AR methods to Component base class.

---
+++ app/models/library/component.rb
@@ -0,0 +1,27 @@
+# Base class for implementing components of Library Items.
+class Library::Component < ActiveRecord::Base
+  self.abstract_class = true
+
+  attr_accessor :created_by
+
+  belongs_to :item, class_name: "Library::Item", foreign_key: "library_item_id"
+
+  scope :recent, lambda {|num| includes(:item).order("library_items.created_at DESC").limit(num) }
+
+  before_create :create_library_item, unless: :item
+  after_create :update_library_item
+
+  validate :created_by, presence: true
+
+  protected
+
+  def create_library_item
+    item = build_item(created_by: created_by)
+    item.save!
+    self.item = item
+  end
+
+  def update_library_item
+    self.item.update_attributes(component: self)
+  end
+end
--- app/models/library/document.rb
+++ app/models/library/document.rb
@@ -10,33 +10,12 @@
 #  file_size       :integer
 #
 
-class Library::Document < ActiveRecord::Base
+class Library::Document < Library::Component
   file_accessor :file do
     storage_path :generate_file_path
   end
 
-  attr_accessor :created_by
-
-  belongs_to :item, class_name: "Library::Item", foreign_key: "library_item_id"
-
-  scope :recent, lambda {|num| includes(:item).order("library_items.created_at DESC").limit(num) }
-
-  before_create :create_library_item, unless: :item
-  after_create :update_library_item
-
-  validate :created_by, presence: true
-
   protected
-
-  def create_library_item
-    item = build_item(created_by: created_by)
-    item.save!
-    self.item = item
-  end
-
-  def update_library_item
-    self.item.update_attributes(component: self)
-  end
 
   def generate_file_path
     hash = Digest::SHA1.file(file.path).hexdigest
