d182d045624e40a00fa0396454d5cfe3c599f503
Andy Allan <andy@gravitystorm.co.uk>
2011-11-16 10:32:11 +0000

    Check for location editing and deleting too

--- spec/requests/user/locations_spec.rb
+++ spec/requests/user/locations_spec.rb
@@ -24,5 +24,26 @@ describe "User locations" do
       page.should have_content("Location Created")
       page.should have_content("Route to School")
     end
+
+    context "edit" do
+      let!(:location) { FactoryGirl.create(:user_location, user: current_user, category: location_category) }
+
+      it "should let you edit an existing location" do
+        visit user_locations_path
+        click_on "Edit" # hmm, edit the right one?
+
+        page.should have_content("Edit Location")
+        find("#user_location_loc_json").set(location_attributes[:loc_json])
+        click_on "Save"
+
+        page.should have_content("Location Updated")
+      end
+
+      it "should let you delete a location" do
+        visit user_locations_path
+        click_on "Delete"
+        page.should have_content("Location deleted")
+      end
+    end
   end
 end
