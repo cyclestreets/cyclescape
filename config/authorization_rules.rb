fc274b578ff6012165f7fc4cf36be0b5d0b20a9c
Andy Allan <andy@gravitystorm.co.uk>
2011-11-17 14:51:50 +0000

    don't let users delete threads

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -37,7 +37,7 @@ authorization do
       if_attribute committee_members: contains { user }
     end
     has_permission_on :issues, to: [:new, :create, :vote_up, :vote_down]
-    has_permission_on :message_threads, :messages, to: :manage
+    has_permission_on :message_threads, :messages, to: [:new, :create]
     has_permission_on :message_thread_subscriptions, to: [:create, :destroy]
     has_permission_on :message_photos, :message_links, :message_deadlines, :message_library_items, to: :create
     has_permission_on :libraries, :library_documents, :library_notes, to: :manage
