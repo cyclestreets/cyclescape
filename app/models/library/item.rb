89dad60146022a1f2705a205a9cab16173f3db1c
Andrew France <andrew@avito.co.uk>
2011-11-25 13:10:55 +0000

    Add tag support to library item model.

--- app/models/library/item.rb
+++ app/models/library/item.rb
@@ -14,9 +14,11 @@
 
 class Library::Item < ActiveRecord::Base
   include FakeDestroy
+  include Taggable
 
   belongs_to :component, polymorphic: true
   belongs_to :created_by, class_name: "User"
+  has_and_belongs_to_many :tags, join_table: "library_item_tags", foreign_key: "library_item_id"
 
   validates_presence_of :created_by
 
