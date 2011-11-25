61da329b1b452c03a964dac6f7af6bc4272590bd
Andy Allan <andy@gravitystorm.co.uk>
2011-11-25 18:03:39 +0000

    Fade out opencyclemap slightly in order than features stand out better

--- config/initializers/map_layers.rb
+++ config/initializers/map_layers.rb
@@ -1,3 +1,3 @@
 module MapLayers
-  OPENCYCLEMAP = OpenLayers::Layer::OSM.new("OpenCycleMap", ["a", "b", "c"].map {|k| "http://#{k}.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"})
+  OPENCYCLEMAP = OpenLayers::Layer::OSM.new("OpenCycleMap", ["a", "b", "c"].map {|k| "http://#{k}.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"}, {opacity: 0.8})
 end
