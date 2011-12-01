fef9da2c4e39fabf01bb6ce25b08b659f40bbe72
Andrew France <andrew@avito.co.uk>
2011-12-01 17:56:09 +0000

    Remove tag migration code which generates the error just fixed elsewhere.

--- db/migrate/20111125111841_convert_issue_categories_to_tags.rb
+++ db/migrate/20111125111841_convert_issue_categories_to_tags.rb
@@ -3,12 +3,6 @@ class ConvertIssueCategoriesToTags < ActiveRecord::Migration
     # Safe way to migrate data while removing category relation
     categories = execute("SELECT i.id, ic.name FROM issues i JOIN issue_categories ic ON i.category_id = ic.id").values
 
-    # Create tags in advance to try to solve migration failure on production that looks
-    # like a transactional issue.
-    categories.each do |cat|
-      Tag.find_or_create_by_name(cat.second)
-    end
-
     Issue.all.each do |issue|
       issue.update_attributes(tags_string: categories.detect {|c| c[0].to_i == issue.id }.second)
     end
