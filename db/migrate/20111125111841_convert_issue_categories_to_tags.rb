e77e57d55198851aa6a773e2328d8348f08b73d4
Andrew France <andrew@avito.co.uk>
2011-12-01 15:02:36 +0000

    Try to fix issue categories to tags migration on production.

--- db/migrate/20111125111841_convert_issue_categories_to_tags.rb
+++ db/migrate/20111125111841_convert_issue_categories_to_tags.rb
@@ -2,6 +2,13 @@ class ConvertIssueCategoriesToTags < ActiveRecord::Migration
   def up
     # Safe way to migrate data while removing category relation
     categories = execute("SELECT i.id, ic.name FROM issues i JOIN issue_categories ic ON i.category_id = ic.id").values
+
+    # Create tags in advance to try to solve migration failure on production that looks
+    # like at transactional issue.
+    categories.each do |cat|
+      Tag.find_or_create_by_name(cat.second)
+    end
+
     Issue.all.each do |issue|
       issue.update_attributes(tags_string: categories.detect {|c| c[0].to_i == issue.id }.second)
     end
