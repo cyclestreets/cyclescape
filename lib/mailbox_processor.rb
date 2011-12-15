ab5552f2897812c0d207b70b9ada6b1b08db5b97
Andrew France <andrew@avito.co.uk>
2011-12-15 12:23:05 +0000

    Refactor mailbox reader to generic mailbox processor.

---
+++ lib/mailbox_processor.rb
@@ -0,0 +1,51 @@
+class MailboxProcessor
+  attr_accessor :config
+
+  def self.process_all_mailboxes
+    mailboxes_config.each do |name, config|
+      reader = self.new(config)
+      reader.run
+    end
+  end
+
+  def self.mailboxes_config
+    return @config if @config
+    config_path = Rails.root + "config" + "mailboxes.yml"
+    raise "Mailboxes config file not found at #{config_path}" unless config_path.exist?
+    @config ||= YAML::load(File.read(config_path)).with_indifferent_access
+  end
+
+  def initialize(config = {})
+    @config = config
+  end
+
+  def establish_connection
+    @imap = Net::IMAP.new(config[:host])
+    @imap.authenticate(config[:authentication], config[:user_name], config[:password])
+    @imap
+  end
+
+  def imap
+    @imap || establish_connection
+  end
+
+  def disconnect
+    @imap.disconnect if @imap and not @imap.disconnected?
+  end
+
+  def fetch_message_ids(mailbox, search_command = "UNSEEN")
+    imap.select(mailbox)
+    imap.uid_search(Array(search_command))
+  end
+
+  def fetch_raw_message(uid)
+    message_text = imap.uid_fetch(uid, ["RFC822"]).first.attr["RFC822"]
+    # Mark message as unseen in case something goes wrong
+    imap.uid_store(uid, "-FLAGS", [:Seen])
+    message_text
+  end
+
+  def mark_as_seen(uid)
+    imap.uid_store(uid, "+FLAGS", [:Seen])
+  end
+end
