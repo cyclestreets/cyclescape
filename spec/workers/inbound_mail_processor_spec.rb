f1cb84fba8660e47f4f6dc7be7e60da3698573c9
Andrew France <andrew@avito.co.uk>
2011-12-16 12:21:52 +0000

    Add inbound mail processor notification test.

--- spec/workers/inbound_mail_processor_spec.rb
+++ spec/workers/inbound_mail_processor_spec.rb
@@ -16,21 +16,34 @@ describe InboundMailProcessor do
     let(:email_recipient) { "thread-#{thread.public_token}@cyclescape.org" }
     let(:inbound_mail) { FactoryGirl.create(:inbound_mail, to: email_recipient) }
 
-    before do
-      subject.perform(inbound_mail.id)
+    context "message" do
+      before do
+        subject.perform(inbound_mail.id)
+      end
+
+      it "should create a new message on the thread" do
+        thread.should have(1).message
+      end
+
+      it "should have the same text as the email" do
+        # There are weird newline issues here, each \r is duplicated in the model's response
+        thread.messages.first.body.gsub(/\n|\r/, '').should == inbound_mail.message.body.to_s.gsub(/\n|\r/, '')
+      end
+
+      it "should have be created by a new user with the email address" do
+        thread.messages.first.created_by.email.should == inbound_mail.message.from.first
+      end
     end
 
-    it "should create a new message on the thread" do
-      thread.should have(1).message
-    end
-
-    it "should have the same text as the email" do
-      # There are weird newline issues here, each \r is duplicated in the model's response
-      thread.messages.first.body.gsub(/\n|\r/, '').should == inbound_mail.message.body.to_s.gsub(/\n|\r/, '')
-    end
-
-    it "should have be created by a new user with the email address" do
-      thread.messages.first.created_by.email.should == inbound_mail.message.from.first
+    context "notifications" do
+      it "should be sent out" do
+        ThreadNotifier.should_receive(:notify_subscribers) do |thread, type, message|
+          thread.should be_a(MessageThread)
+          type.should == :new_message
+          message.should be_a(Message)
+        end
+        subject.perform(inbound_mail.id)
+      end
     end
   end
 end
