d5a792a84264520ea01dfec2ceaeadeb910e6820
Andy Allan <andy@gravitystorm.co.uk>
2012-01-03 18:10:26 +0000

    Move to black-and-white theme from designers

--- app/assets/javascripts/map_style.js
+++ app/assets/javascripts/map_style.js
@@ -1,18 +1,19 @@
 MapStyle = {
   displayRules: [new OpenLayers.Rule({
       symbolizer: {"Line" : { strokeWidth: 8,
-                              strokeColor: '#00ff00',
-                              strokeOpacity: 0.8 },
+                              strokeColor: '#333333',
+                              strokeOpacity: 0.9 },
                     "Point" : { pointRadius: 8,
-                                strokeColor: '#004400',
+                                strokeColor: '#333333',
                                 strokeOpacity: 0.9,
                                 strokeWidth: 2,
-                                fillOpacity: 0.3,
-                                fillColor: '#00ff00' },
+                                fillOpacity: 0.4,
+                                fillColor: '#ffffff' },
                     "Polygon" : { strokeWidth: 2.5,
-                                  strokeOpacity: 0.8,
-                                  strokeColor: '#004400',
-                                  fillColor: '#00ff00' }
+                                  strokeOpacity: 0.9,
+                                  strokeColor: '#333333',
+                                  fillOpacity: 0.5,
+                                  fillColor: '#ffffff' }
       },
       elseFilter: true
   })],
@@ -26,11 +27,11 @@ MapStyle = {
                                 strokeOpacity: 0.9,
                                 strokeWidth: 2,
                                 fillOpacity: 0.3,
-                                fillColor: '#000000' },
+                                fillColor: '#ffffff' },
                     "Polygon" : { strokeWidth: 2.5,
                                   strokeOpacity: 0.8,
                                   strokeColor: '#000000',
-                                  fillColor: '#000000' }
+                                  fillColor: '#ffffff' }
       },
       elseFilter: true
   })],
