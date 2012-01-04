fc8aff61081b281d2fe540bbef7852126e3398f1
Andrew France <andrew@avito.co.uk>
2012-01-04 16:44:24 +0000

    Forgot to add decorator init. Bit of tidy up.

--- app/decorators/issue_decorator.rb
+++ app/decorators/issue_decorator.rb
@@ -1,6 +1,14 @@
 class IssueDecorator < ApplicationDecorator
   decorates :issue
 
+  def map
+    h.render partial: "map", locals: {issue: issue}
+  end
+
+  def description
+    h.simple_format issue.description
+  end
+
   def standard_photo_image
     return "" if photo.nil?
     h.image_tag standard_photo_url, class: "issue-photo"
