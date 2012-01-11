d7bac0a8f9e2c194465f36c7eef8f6b0197ab423
Andrew France <andrew@avito.co.uk>
2012-01-11 15:03:45 +0000

    Refactor thread email message creation and add parsing.
    Should strip out signatures and unneeded reply blocks.

--- app/workers/inbound_mail_processor.rb
+++ app/workers/inbound_mail_processor.rb
@@ -11,27 +11,11 @@ class InboundMailProcessor
   end
 
   def self.deliver_thread_reply(mail, thread_token)
-    from_address = mail.message.header[:from].addresses.first
-    from_name = mail.message.header[:from].display_names.first
-
     thread = MessageThread.find_by_public_token(thread_token)
     raise "Thread #{thread_token.inspect} not found" if thread.nil?
 
-    user = User.find_or_invite(from_address, from_name)
-    raise "Invalid user" if user.nil?
-
-    # For multipart messages we pull out the text/plain content
-    body = if mail.message.multipart?
-      mail.message.text_part.body
-    else
-      mail.message.body
-    end
-
-    message = thread.messages.build
-    message.body = body.to_s
-    message.created_by = user
-    message.save!
-
+    # This raises an exception if it fails
+    message = thread.add_message_from_email!(mail)
     ThreadNotifier.notify_subscribers(thread, :new_message, message)
   end
 end
