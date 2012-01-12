3fefbb18bc4deefbc99f0222e4cd48f624396e0d
Andy Allan <andy@gravitystorm.co.uk>
2012-01-12 10:36:57 +0000

    Refactor MessageThreads to use TagPanelDecorator.

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -12,6 +12,7 @@ class MessageThreadsController < ApplicationController
     @new_message = @thread.messages.build
     @subscribers = @thread.subscribers
     @library_items = Library::Item.find_by_tags_from(@thread).limit(5)
+    @tag_panel = TagPanelDecorator.new(@thread, form_url: thread_tags_path(@thread))
   end
 
   def edit
