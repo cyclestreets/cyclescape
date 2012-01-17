193e4efb2e761b73d22f355df2943ae9dd90400b
Andrew France <andrew@avito.co.uk>
2012-01-17 18:39:55 +0000

    Main dashboard design.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,10 @@ class DashboardsController < ApplicationController
     @groups = @user.groups
     @relevant_issues = current_user.issues_near_locations.order("updated_at DESC")
     @subscribed_threads = current_user.subscribed_threads.limit(4)
-    @involved_threads = current_user.involved_threads.limit(4)
+    involved_threads = ThreadList.recent_involved_with(current_user, 4)
+    @involved_threads = ThreadListDecorator.decorate(involved_threads)
+
+    group_threads = ThreadList.recent_from_groups(current_user.groups, 4)
+    @group_threads = ThreadListDecorator.decorate(group_threads)
   end
 end
