df66afbe8c6eaca17ea63b69f623ff85b03d86b1
Andrew France <andrew@avito.co.uk>
2012-01-18 16:58:48 +0000

    Issues page pagination with will_paginate.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,7 +2,7 @@ class IssuesController < ApplicationController
   filter_access_to [:edit, :update, :destroy], attribute_check: true
   
   def index
-    @issues = IssueDecorator.decorate(Issue.by_most_recent.limit(10).page(params[:page]))
+    @issues = IssueDecorator.decorate(Issue.by_most_recent.paginate(page: params[:page]))
     @start_location = index_start_location
     @popular_issues = Issue.plusminus_tally(start_at: 2.weeks.ago) # only count recent votes
   end
