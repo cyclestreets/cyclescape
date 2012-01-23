54d7882730af0844bda71281ba34c536ed41509f
Andy Allan <andy@gravitystorm.co.uk>
2012-01-23 15:34:36 +0000

    Test that the group profile page is working.

--- spec/requests/groups/profiles_spec.rb
+++ spec/requests/groups/profiles_spec.rb
@@ -1,6 +1,25 @@
 require "spec_helper"
 
 describe "Group profiles" do
+  context "as a public user" do
+
+    describe "viewing the group profile" do
+      let(:group) { FactoryGirl.create(:group) }
+
+      before do
+        # fixme Work around - hard to get group + group_profile via factories
+        group.profile.description = "This is a group of people."
+        group.profile.location = "POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))"
+        group.profile.save!
+      end
+
+      it "should show you the profile" do
+        visit group_profile_path(group)
+        page.should have_content(group.profile.description)
+      end
+    end
+  end
+
   context "as a group member" do
     include_context "signed in as a group member"
 
