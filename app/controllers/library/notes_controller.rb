a14301c9a2a3fcaeb6f521ef836b09a151a8bca2
Andy Allan <andy@gravitystorm.co.uk>
2012-01-24 15:33:09 +0000

    Show thread listing on library notes.

--- app/controllers/library/notes_controller.rb
+++ app/controllers/library/notes_controller.rb
@@ -22,6 +22,7 @@ class Library::NotesController < ApplicationController
 
   def show
     @tag_panel = TagPanelDecorator.new(@note.item, form_url: library_tag_path(@note.item))
+    @threads = @note.item.threads.public.order_by_latest_message.limit(10)
   end
 
   def edit
