736ebe397ae3675d61108cd3db0c196c341c6329
Andy Allan <andy@gravitystorm.co.uk>
2012-01-25 16:28:39 +0000

    Handle unrecognised subdomains with a redirect to www

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,10 +2,15 @@ class GroupsController < ApplicationController
   def show
     if params[:id]
       @group = Group.find(params[:id])
-    else current_group
+    elsif current_group
       @group = current_group
     end
-    @recent_threads = @group.threads.order("created_at DESC").limit(10)
-    @recent_issues = @group.recent_issues.limit(10)
+
+    if @group
+      @recent_threads = @group.threads.order("created_at DESC").limit(10)
+      @recent_issues = @group.recent_issues.limit(10)
+    else
+      redirect_to root_url(subdomain: 'www')
+    end
   end
 end
