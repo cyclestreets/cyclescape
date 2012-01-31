4022a03c95d920d0b3c3771976a9ff880f109c69
Andy Allan <andy@gravitystorm.co.uk>
2012-01-31 15:18:17 +0000

    Fix bug where denied and cancelled membership requests were being counted, refactor, and add tests.

--- app/controllers/group/membership_requests_controller.rb
+++ app/controllers/group/membership_requests_controller.rb
@@ -14,7 +14,7 @@ class Group::MembershipRequestsController < ApplicationController
   def create
     if current_user.groups.include?(@group)
       redirect_to @group, alert: t(".group.membership_requests.create.already_member")
-    elsif current_user.membership_requests.where(group_id: @group.id).count > 0
+    elsif current_user.membership_request_pending_for?(@group)
       redirect_to @group, alert: t(".group.membership_requests.create.already_asked")
     else
       @request = @group.membership_requests.build({user: current_user})
