032e5019da318a4bcaee9756650b8fb0053ab81d
Andrew France <andrew@avito.co.uk>
2012-01-31 18:03:18 +0000

    Update group threads page and display for current group subdomain.
    * For threads menu the public thread page is display if no group is selected.
    * Displays issue and general threads for the group if current_group set.
    * Pagination support with the tabs.

--- lib/thread_list.rb
+++ lib/thread_list.rb
@@ -7,6 +7,14 @@ class ThreadList
     MessageThread.public.where(group_id: groups).order_by_latest_message.limit(limit)
   end
 
+  def self.issue_threads_from_group(group)
+    group.threads.with_issue
+  end
+
+  def self.general_threads_from_group(group)
+    group.threads.without_issue
+  end
+
   def self.recent_involved_with(user, limit)
     user.involved_threads.order_by_latest_message.limit(limit)
   end
