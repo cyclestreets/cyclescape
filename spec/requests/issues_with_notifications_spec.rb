7126f88d2b615b65a774d950b78acc19dcb991c1
Andy Allan <andy@gravitystorm.co.uk>
2012-02-15 09:49:56 +0000

    Refactor failing test, to ensure only the smallest user location is used, regardless of order they are created in.

--- spec/requests/issues_with_notifications_spec.rb
+++ spec/requests/issues_with_notifications_spec.rb
@@ -38,20 +38,32 @@ describe "Issue notifications" do
         email.should_not have_body_text("&amp;")
         email.should have_body_text(/Test containing A & B/)
       end
+    end
+  end
 
-      context "multiple overlapping locations" do
-        let!(:user_location2) { FactoryGirl.create(:user_location, user: user, loc_json: issue_values[:loc_json]) }
+  context "multiple overlapping locations" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:user_location) { FactoryGirl.create(:user_location, user: user, location: "POLYGON ((-10 41, 10 41, 10 61, -10 61, -10 41))") }
+    let!(:user_location_big) { FactoryGirl.create(:user_location, user: user, location: user_location.location.buffer(1)) }
+    let!(:user_location_small) { FactoryGirl.create(:user_location, user: user, location: user_location.location.buffer(-1)) }
 
-        it "should not send multiple emails to the same user" do
-          email_count = all_emails.count
-          visit new_issue_path
-          fill_in "Title", with: "Test"
-          fill_in "Write a description", with: "Something & something else"
-          find("#issue_loc_json").set(issue_values[:loc_json])
-          click_on "Send Report"
-          all_emails.count.should eql(email_count + 1)
-        end
-      end
+
+    before do
+      user.prefs.update_attribute(:notify_new_user_locations_issue, true)
+    end
+
+    it "should not send multiple emails to the same user" do
+      email_count = all_emails.count
+      visit new_issue_path
+      fill_in "Title", with: "Test"
+      fill_in "Write a description", with: "Something & something else"
+      find("#issue_loc_json").set(user_location.loc_json)
+      click_on "Send Report"
+      all_emails.count.should eql(email_count + 1)
+      open_email(user_location.user.email)
+      current_email.should have_body_text(user_location_small.category.name)
+      current_email.should_not have_body_text(user_location_big.category.name)
+      current_email.should_not have_body_text(user_location.category.name)
     end
   end
 end
