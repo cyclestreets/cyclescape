f471d203ddfc3e2047bce7e37b9f395ec74a4ef8
Andrew France <andrew@avito.co.uk>
2012-03-02 14:28:29 +0000

    Set reply-to on new thread notifications.

--- app/mailers/thread_mailer.rb
+++ app/mailers/thread_mailer.rb
@@ -1,4 +1,5 @@
 class ThreadMailer < ActionMailer::Base
+  include MailerHelper
   default from: Rails.application.config.default_email_from
 
   def new_message(message, subscriber)
@@ -28,12 +29,11 @@ class ThreadMailer < ActionMailer::Base
     @message = message
     @thread = message.thread
     @subscriber = subscriber
-    domain = Rails.application.config.default_email_from_domain
-    email_from = ['"', message.created_by.name, '" <notifications@', domain, ">"]
-    reply_to = ['"', I18n.t(".application_name"), '" <thread-', @thread.public_token, "@", domain, ">"]
+    email_from = user_notification_address(message.created_by)
+    reply_to = thread_address(@thread)
     mail(to: subscriber.name_with_email,
          subject: t("mailers.thread_mailer.common.subject", title: @thread.title),
-         from: email_from.join,
-         reply_to: reply_to.join)
+         from: email_from,
+         reply_to: reply_to)
   end
 end
