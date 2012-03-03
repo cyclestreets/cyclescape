a31ae6c37fe5ecbbd1c8c38ee39b48d5023fd08b
Andrew France <andrew@avito.co.uk>
2012-03-03 13:39:21 +0000

    Issues should use a sequence for the title so they are unique lists.

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -15,7 +15,7 @@
 
 FactoryGirl.define do
   factory :issue do
-    title "There's something wrong"
+    sequence(:title) {|n| "Issue #{n}" }
     description "Whose leg do you have to hump to get a dry martini around here?"
     location "POINT(-122 47)"
     association :created_by, factory: :user
