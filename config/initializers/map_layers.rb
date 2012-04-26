3dbf92c2b6f80c87d457579a2d63559a02104442
Andy Allan <andy@gravitystorm.co.uk>
2012-04-26 10:40:44 +0100

    Update tile layers to add crossOrigin overrides.
    OpenCycleMap has been updated to serve cross-origin headers, but will take a few days for the caches to update. This can be removed for the OpenCycleMap layer in a couple of week's time.

--- config/initializers/map_layers.rb
+++ config/initializers/map_layers.rb
@@ -1,4 +1,6 @@
 module MapLayers
-  OPENCYCLEMAP = OpenLayers::Layer::OSM.new("OpenCycleMap", ["a", "b", "c"].map {|k| "http://#{k}.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"}, {opacity: 0.8})
-  OS_STREETVIEW = OpenLayers::Layer::OSM.new("OS StreetView", ["a", "b", "c"].map {|k| "http://#{k}.os.openstreetmap.org/sv/${z}/${x}/${y}.png"}, {opacity: 0.8})
+  OPENCYCLEMAP = OpenLayers::Layer::OSM.new("OpenCycleMap", ["a", "b", "c"].map {|k| "http://#{k}.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"},
+                                            {opacity: 0.8, tileOptions: {crossOriginKeyword: :null}})
+  OS_STREETVIEW = OpenLayers::Layer::OSM.new("OS StreetView", ["a", "b", "c"].map {|k| "http://#{k}.os.openstreetmap.org/sv/${z}/${x}/${y}.png"},
+                                             {opacity: 0.8, tileOptions: {crossOriginKeyword: :null}})
 end
