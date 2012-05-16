96e7138085b9fedaa602522b390a9e5c3efda393
Andy Allan <andy@gravitystorm.co.uk>
2012-05-16 11:39:57 +0100

    Remove unused with_pref method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,10 +76,6 @@ class User < ActiveRecord::Base
       each {|u| u.create_user_prefs }
   end
 
-  def self.with_pref(name)
-    joins(:prefs).where(user_prefs: {name => true})
-  end
-
   def name
     return display_name unless display_name.blank?
     full_name
