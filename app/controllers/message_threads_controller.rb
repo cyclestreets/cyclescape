586aad820b98d839f302231179b6283121704a2a
Andy Allan <andy@gravitystorm.co.uk>
2012-05-16 13:51:14 +0100

    Only auto-subscribe to threads if the user has permission to read the thread.

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -61,7 +61,11 @@ class MessageThreadsController < ApplicationController
     pref = t[:involve_my_groups].eq("subscribe")
     constraint = thread.issue ? pref : pref.and(t[:involve_my_groups_admin].eq(true))
     members = thread.group.members.active.joins(:prefs).where(constraint)
-    members.each{ |member| thread.subscriptions.create(user: member) unless member.subscribed_to_thread?(thread) }
+    members.each do |member|
+      if Authorization::Engine.instance.permit? :show, { object: thread, user: member }
+        thread.subscriptions.create(user: member) unless member.subscribed_to_thread?(thread)
+      end
+    end
   end
 
   def subscribe_issue_users(thread)
@@ -72,6 +76,10 @@ class MessageThreadsController < ApplicationController
         where(user_prefs: {involve_my_locations: "subscribe"}).
         all
 
-    locations.each{ |loc| thread.subscriptions.create(user: loc.user) unless loc.user.subscribed_to_thread?(thread) }
+    locations.each do |loc|
+      if Authorization::Engine.instance.permit? :show, { object: thread, user: loc.user }
+        thread.subscriptions.create(user: loc.user) unless loc.user.subscribed_to_thread?(thread)
+      end
+    end
   end
 end
