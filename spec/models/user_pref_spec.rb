6235274f9994f10578eb5658ec9deb7e3bb4c5d0
Andy Allan <andy@gravitystorm.co.uk>
2012-05-16 10:46:49 +0100

    Update model specs for user preferences.

--- spec/models/user_pref_spec.rb
+++ spec/models/user_pref_spec.rb
@@ -20,17 +20,23 @@ describe UserPref do
 
   describe "attributes" do
     booleans = %w(
-      notify_subscribed_threads
-      notify_new_user_locations_issue
-      notify_new_group_thread
-      notify_new_group_location_issue
-      notify_new_user_locations_issue_thread
-      subscribe_new_group_thread
-      subscribe_new_user_location_issue_thread
+      involve_my_groups_admin
+      enable_email
       )
 
     booleans.each do |attr|
       it "should respond to #{attr} with true or false" do
+        subject.send(attr).should_not be_nil
+      end
+    end
+
+    strings = %w(
+      involve_my_locations
+      involve_my_groups
+      )
+
+    strings.each do |attr|
+      it "should respond to #{attr} with a default value" do
         subject.send(attr).should_not be_nil
       end
     end
