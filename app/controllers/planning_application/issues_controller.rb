c218f309347e7aef8d871d633aee247db5a3eac7
Andy Allan <andy@gravitystorm.co.uk>
2012-05-31 10:06:35 +0100

    When creating an issue from a planning application, ensure the planning application has a link to the issue.

--- app/controllers/planning_application/issues_controller.rb
+++ app/controllers/planning_application/issues_controller.rb
@@ -2,12 +2,26 @@ class PlanningApplication::IssuesController < IssuesController
   before_filter :load_planning_application
 
   def new
-    @issue = Issue.new
+    @issue = @planning_application.build_issue
     @issue.title = @planning_application.title #truncated
     @issue.location = @planning_application.location
     @issue.description = @planning_application.description
     @issue.tags_string = "planning"
     @start_location = @planning_application.location
+  end
+
+  def create
+    @issue = current_user.issues.new(params[:issue])
+
+    if @issue.save
+      @planning_application.issue = @issue
+      @planning_application.save!
+      NewIssueNotifier.new_issue(@issue)
+      redirect_to @issue
+    else
+      @start_location = current_user.start_location
+      render :new
+    end
   end
 
   protected
