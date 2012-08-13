00b4e53f9ceee4c06b2ace4589e5b1a2e2d0e83a
Andy Allan <andy@gravitystorm.co.uk>
2012-08-13 11:08:45 +0100

    Preload issues, group and issue-created-by information for group homepage.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,12 +9,12 @@ class GroupsController < ApplicationController
     if @group
       @group = GroupDecorator.decorate(@group)
       if @group.has_member?(current_user)
-        recent_threads = ThreadList.recent_from_groups(@group, 10)
+        recent_threads = ThreadList.recent_from_groups(@group, 10).includes(:issue, :group)
       else
-        recent_threads = ThreadList.recent_public_from_groups(@group, 10)
+        recent_threads = ThreadList.recent_public_from_groups(@group, 10).includes(:issue, :group)
       end
       @recent_threads = ThreadListDecorator.decorate(recent_threads)
-      @recent_issues = IssueDecorator.decorate(@group.recent_issues.limit(10))
+      @recent_issues = IssueDecorator.decorate(@group.recent_issues.limit(10).includes(:created_by))
     else
       redirect_to root_url(subdomain: 'www')
     end
