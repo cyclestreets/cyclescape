4fb132022e71037780daf7ad75656da71a06a6d6
Andy Allan <andy@gravitystorm.co.uk>
2012-11-01 17:33:37 +0000

    Add a progress bar, suppress activerecord logging, and wrap in a transaction.

--- lib/planning_applications_processor.rb
+++ lib/planning_applications_processor.rb
@@ -3,6 +3,7 @@ class PlanningApplicationsProcessor
 require 'net/http'
 require 'tempfile'
 require 'csv'
+require 'progressbar'
 
   def self.process_planning_applications
     processor = self.new
@@ -88,22 +89,34 @@ require 'csv'
 #   end
   
   def process_csv
-    @csv_path = '/home/andy/temp/cyclestreets/planning applications/planning_applications.csv'
-    count = 0
-    csv = CSV.foreach(@csv_path, headers: true, header_converters: :symbol) do |row|
-      record = PlanningApplication.find_by_openlylocal_id(row[:openlylocal_id])
-      record = PlanningApplication.new unless record
-      [:openlylocal_id, :openlylocal_url, :address, :postcode, :description, :council_name, :url, :uid].each do |symbol|
-        record[symbol] = row[symbol]
+    @csv_path = '/home/andy/temp/cyclestreets/planning applications/planning_applications.head10k.csv'
+
+    # use wc for optimal speed :-)
+    csv_length = %x{wc -l "#{@csv_path}"}.to_i
+
+    # Create a progress bar using the length of the file
+    pbar = ProgressBar.new("Updating", csv_length)
+
+    # Suppress the activerecord logging, so that we can see the progressbar
+    ActiveRecord::Base.logger = Logger.new('/dev/null')
+
+    ActiveRecord::Base.transaction do
+      csv = CSV.foreach(@csv_path, headers: true, header_converters: :symbol) do |row|
+        record = PlanningApplication.find_by_openlylocal_id(row[:openlylocal_id])
+        record = PlanningApplication.new unless record
+        [:openlylocal_id, :openlylocal_url, :address, :postcode, :description, :council_name, :url, :uid].each do |symbol|
+          record[symbol] = row[symbol]
+        end
+        record[:openlylocal_council_url] = row[:council_openlylocal_url]
+        record.location = "POINT(#{row[:lng]} #{row[:lat]})"
+        # Unhandled symbols:
+        # updated_at retrieved_at decision status date_received date_validated start_date
+        record.save!
+        pbar.inc
       end
-      record[:openlylocal_council_url] = row[:council_openlylocal_url]
-      record.location = "POINT(#{row[:lng]} #{row[:lat]})"
-      # Unhandled symbols:
-      # updated_at retrieved_at decision status date_received date_validated start_date
-      record.save!
-      count += 1
-      puts count
     end
+
+    pbar.finish
   end
 
   def cleanup
