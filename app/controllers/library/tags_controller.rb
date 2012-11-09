812d1edc264a0850f5af96cee18c9ce181f3010b
Andy Allan <andy@gravitystorm.co.uk>
2012-11-09 14:55:09 +0000

    Supply the correct context to the library tag update response. Fixes #110

--- app/controllers/library/tags_controller.rb
+++ app/controllers/library/tags_controller.rb
@@ -2,7 +2,7 @@ class Library::TagsController < ApplicationController
   def update
     @item = Library::Item.find(params[:id])
     if @item.update_attributes(tags_string: params[:library_item][:tags_string])
-      render text: TagPanelDecorator.new(@item, form_url: url_for).render
+      render text: TagPanelDecorator.new(@item, form_url: url_for, auth_context: :library_tags).render
     else
       head :conflict
     end
