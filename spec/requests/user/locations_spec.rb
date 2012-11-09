94a3eefced0eb5f1ee68c1b896f5e28e1e49ce72
Andy Allan <andy@gravitystorm.co.uk>
2012-11-09 15:41:02 +0000

    Given we know what the users location preferences are, we can give appropriate guidance.

--- spec/requests/user/locations_spec.rb
+++ spec/requests/user/locations_spec.rb
@@ -25,6 +25,28 @@ describe "User locations" do
       page.should have_content(location_category.name)
     end
 
+    context "with a location" do
+      let!(:location) { FactoryGirl.create(:user_location, user: current_user) }
+
+      def guidance_text(key)
+        I18n.t(".user.locations.index#{key}", edit_prefs_link: I18n.t(".user.locations.index.edit_your_prefs"))
+      end
+
+      it "should give appropriate guidance based on your settings" do
+        current_user.prefs.update_column(:involve_my_locations, "none")
+        visit user_locations_path
+        page.should have_content(guidance_text('.combined_locations_guidance_none_html'))
+
+        current_user.prefs.update_column(:involve_my_locations, "notify")
+        visit user_locations_path
+        page.should have_content(guidance_text('.combined_locations_guidance_notify_html'))
+
+        current_user.prefs.update_column(:involve_my_locations, "subscribe")
+        visit user_locations_path
+        page.should have_content(guidance_text('.combined_locations_guidance_subscribe_html'))
+      end
+    end
+
     context "edit" do
       let!(:location) { FactoryGirl.create(:user_location, user: current_user, category: location_category) }
 
