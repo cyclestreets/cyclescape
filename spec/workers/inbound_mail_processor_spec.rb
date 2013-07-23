883cb728234c22c0c792725ee9be031e92a862a7
Andy Allan <andy@gravitystorm.co.uk>
2013-07-23 17:54:11 +0100

    Newline handling has changed slightly in new versions of mail. Update tests accordingly.

--- spec/workers/inbound_mail_processor_spec.rb
+++ spec/workers/inbound_mail_processor_spec.rb
@@ -53,7 +53,7 @@ describe InboundMailProcessor do
 
       it "should have the same text as the email text part" do
         message_body = thread.messages.first.body
-        message_body.should == "\n\nOn Tue, 20 Dec 2011, Cyclescape wrote:\n\n> Robin Bird added a message to the thread.\n>\n> I believe the idea is that 20m will be used to work out what to do and\n> how much that would cost. I therefore think that we do need to push for\n> cycle infrastructure along the A14 as a way of allowing them to justify\n> not widening the road quiet so much.\nI think the £20m is actually to implement things though, not a feasibility\nstudy. The current consultation seems to be about asking people what the\n£20m should be spent on:\n\nhttp://www.dft.gov.uk/consultations/dft-20111212\n\n\"schemes delivered over the next two years\""
+        message_body.should == "\nOn Tue, 20 Dec 2011, Cyclescape wrote:\n\n> Robin Bird added a message to the thread.\n>\n> I believe the idea is that 20m will be used to work out what to do and\n> how much that would cost. I therefore think that we do need to push for\n> cycle infrastructure along the A14 as a way of allowing them to justify\n> not widening the road quiet so much.\nI think the £20m is actually to implement things though, not a feasibility\nstudy. The current consultation seems to be about asking people what the\n£20m should be spent on:\n\nhttp://www.dft.gov.uk/consultations/dft-20111212\n\n\"schemes delivered over the next two years\"\n"
       end
     end
 
@@ -70,7 +70,7 @@ describe InboundMailProcessor do
 
       it "should have the same text as the email text part" do
         message_body = thread.messages.first.body
-        message_body.should == "\n\nOn Tue, 20 Dec 2011, Cyclescape wrote:\n\n> Robin Bird added a message to the thread.\n>\n> I believe the idea is that 20m will be used to work out what to do and\n> how much that would cost. I therefore think that we do need to push for\n> cycle infrastructure along the A14 as a way of allowing them to justify\n> not widening the road quiet so much.\nI think the £20m is actually to implement things though, not a feasibility\nstudy. The current consultation seems to be about asking people what the\n£20m should be spent on:\n\nhttp://www.dft.gov.uk/consultations/dft-20111212\n\n\"schemes delivered over the next two years\""
+        message_body.should == "\nOn Tue, 20 Dec 2011, Cyclescape wrote:\n\n> Robin Bird added a message to the thread.\n>\n> I believe the idea is that 20m will be used to work out what to do and\n> how much that would cost. I therefore think that we do need to push for\n> cycle infrastructure along the A14 as a way of allowing them to justify\n> not widening the road quiet so much.\nI think the £20m is actually to implement things though, not a feasibility\nstudy. The current consultation seems to be about asking people what the\n£20m should be spent on:\n\nhttp://www.dft.gov.uk/consultations/dft-20111212\n\n\"schemes delivered over the next two years\"\n"
       end
     end
 
@@ -87,7 +87,7 @@ describe InboundMailProcessor do
 
       it "should have the first message as the plain text part" do
         message_body = thread.messages[0].body
-        message_body.should == "This email has an attached image.\n\nAndy"
+        message_body.should == "This email has an attached image.\n\nAndy\n\n"
       end
 
       it "should have the second image as a photo message" do
@@ -112,7 +112,7 @@ describe InboundMailProcessor do
 
       it "should have the first message as the plain text part" do
         message_body = thread.messages[0].body
-        message_body.should == "This email has an attached file.\n\nAndy"
+        message_body.should == "This email has an attached file.\n\nAndy\n\n"
       end
 
       it "should have the second message as an attachment message" do
