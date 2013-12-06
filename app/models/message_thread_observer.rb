fceaae75f7506ad0667aacaa9d55ffe59a6fb4a8
Andy Allan <andy@gravitystorm.co.uk>
2013-12-06 18:24:24 +0000

    Add reminders that notifications need sending too.

--- app/models/message_thread_observer.rb
+++ app/models/message_thread_observer.rb
@@ -4,18 +4,26 @@ class MessageThreadObserver < ActiveRecord::Observer
       case thread.privacy
       when "committee"
         thread.subscribers.each do |subscriber|
-          subscriber.thread_subscriptions.to(thread).destroy unless thread.group.committee_members.include?(subscriber)
+          unless thread.group.committee_members.include?(subscriber)
+            subscriber.thread_subscriptions.to(thread).destroy
+            # TODO add removal notification
+          end
         end
       when "group"
         if thread.privacy_was == "public"
           thread.subscribers.each do |subscriber|
-            subscriber.thread_subscriptions.to(thread).destroy unless thread.group.members.include?(subscriber)
+            unless thread.group.members.include?(subscriber)
+              subscriber.thread_subscriptions.to(thread).destroy
+              # TODO add removal notification
+            end
           end
         elsif thread.privacy_was == "committee"
           ThreadSubscriber.subscribe_users(thread)
+          # TODO add available notification for involvegroups / locations :notify
         end
       when "public"
         ThreadSubscriber.subscribe_users(thread)
+        # TODO add notification for involvegroups / locations :notify
       end
     end
   end
