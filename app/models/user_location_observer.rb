22605daa26b5323b10f3e13e20ce3ea61433025a
Andy Allan <andy@gravitystorm.co.uk>
2013-12-18 19:50:08 +0000

    Add a UserLocation observer, and subscribe to relevant issue threads when created or saved.

---
+++ app/models/user_location_observer.rb
@@ -0,0 +1,21 @@
+class UserLocationObserver < ActiveRecord::Observer
+
+  def after_save(user_location)
+    user = user_location.user
+    if user.prefs.involve_my_locations == "subscribe"
+      Issue.intersects(user_location.location).each do |issue|
+        issue.threads.each do |thread|
+          if permissions_check(user, thread) && !user.ever_subscribed_to_thread?(thread)
+            thread.add_subscriber(user)
+          end
+        end
+      end
+    end
+  end
+
+  private
+
+  def permissions_check(user, thread)
+    Authorization::Engine.instance.permit? :show, { object: thread, user: user, user_roles: [:member, :guest] }
+  end
+end
---
+++ spec/models/user_location_observer_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe UserLocationObserver do
+  subject { UserLocationObserver.instance }
+
+  context "basic checks" do
+    let(:ul) { FactoryGirl.build(:user_location) }
+
+    it "should notice when UserLocations are saved" do
+      subject.should_receive(:after_save)
+
+      UserLocation.observers.enable :user_location_observer do
+        ul.save
+      end
+    end
+  end
+
+  context "adding a location" do
+    let(:issue) { FactoryGirl.create(:issue) }
+    let!(:thread) { FactoryGirl.create(:issue_message_thread, issue: issue) }
+    let(:user_location) { FactoryGirl.build(:user_location, location: issue.location) }
+    let(:user) { user_location.user }
+    let(:group) { FactoryGirl.create(:group) }
+
+    context "with pref" do
+      before do
+        user.prefs.update_column(:involve_my_locations, "subscribe")
+      end
+
+      it "should subscribe users to existing threads" do
+        thread.subscribers.should_not include(user)
+        UserLocation.observers.enable :user_location_observer do
+          user_location.save
+        end
+        thread.reload
+        thread.subscribers.should include(user)
+      end
+
+      it "should not subscribe users to private threads" do
+        group.members.should_not include(user)
+        thread.group = group
+        thread.privacy = "group"
+        thread.save
+        UserLocation.observers.enable :user_location_observer do
+          user_location.save
+        end
+        thread.reload
+        thread.subscribers.should_not include(user)
+      end
+    end
+
+    context "without pref" do
+      before do
+        user.prefs.update_column(:involve_my_locations, "notify")
+      end
+
+      it "should not subscribe users to existing threads" do
+        thread.subscribers.should_not include(user)
+        UserLocation.observers.enable :user_location_observer do
+          user_location.save
+        end
+        thread.reload
+        thread.subscribers.should_not include(user)
+      end
+    end
+  end
+end
