b9f8c6de23de5dddac908ea92750f2c4e08f6051
Andy Allan <andy@gravitystorm.co.uk>
2014-03-20 12:45:19 +0000

    When you change your pref regarding group admin discussions, subscribe to the relevant threads.

--- app/models/user_pref_observer.rb
+++ app/models/user_pref_observer.rb
@@ -1,2 +1,20 @@
 class UserPrefObserver < ActiveRecord::Observer
+  def after_save(pref)
+    if pref.involve_my_groups_admin_changed?
+      if pref.involve_my_groups_admin
+        user = pref.user
+        user.groups.each do |group|
+          group.threads.without_issue.each do |thread|
+            if permissions_check(user, thread) && !user.ever_subscribed_to_thread?(thread)
+              thread.add_subscriber(user)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def permissions_check(user, thread)
+    Authorization::Engine.instance.permit? :show, object: thread, user: user, user_roles: [:member, :guest]
+  end
 end
---
+++ spec/models/user_pref_observer_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe UserPrefObserver do
+  subject { UserPrefObserver.instance }
+
+  context 'basic checks' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    it 'should notice when UserPrefs are saved' do
+      user # triggers initial creation events which we want to ignore
+
+      subject.should_receive(:after_save)
+      UserPref.observers.enable :user_pref_observer do
+        user.prefs.save
+      end
+    end
+  end
+
+  context 'administrative discussions' do
+    context 'when enabling pref' do
+      let(:group_membership) { FactoryGirl.create(:group_membership) }
+      let(:group_thread) { FactoryGirl.create(:message_thread, group: group_membership.group) }
+      let(:group_issue_thread) { FactoryGirl.create(:issue_message_thread, group: group_membership.group) }
+      let(:group_private_thread) { FactoryGirl.create(:message_thread, group: group_membership.group, privacy: 'committee') }
+      let(:user) { group_membership.user }
+
+      it 'should subscribe to group administrative threads' do
+        group_thread.subscribers.should_not include(user)
+        UserPref.observers.enable :user_pref_observer do
+          user.prefs.involve_my_groups_admin = true
+          user.prefs.save
+        end
+        group_thread.reload
+        group_thread.subscribers.should include(user)
+      end
+
+      it 'should not subscribe to group issue threads' do
+        group_issue_thread.subscribers.should_not include(user)
+        UserPref.observers.enable :user_pref_observer do
+          user.prefs.involve_my_groups_admin = true
+          user.prefs.save
+        end
+        group_issue_thread.reload
+        group_issue_thread.subscribers.should_not include(user)
+      end
+
+      it 'should not subscribe to committee admin threads if user not on committee' do
+        group_private_thread.subscribers.should_not include(user)
+        UserPref.observers.enable :user_pref_observer do
+          user.prefs.involve_my_groups_admin = true
+          user.prefs.save
+        end
+        group_private_thread.reload
+        group_private_thread.subscribers.should_not include(user)
+      end
+
+      it 'should not subscribe to threads that have been previously unsubscribed' do
+        group_thread.add_subscriber(user)
+        user.thread_subscriptions.to(group_thread).destroy
+        group_thread.subscribers.should_not include(user)
+        UserPref.observers.enable :user_pref_observer do
+          user.prefs.involve_my_groups_admin = true
+          user.prefs.save
+        end
+        group_thread.reload
+        group_thread.subscribers.should_not include(user)
+      end
+    end
+  end
+end
