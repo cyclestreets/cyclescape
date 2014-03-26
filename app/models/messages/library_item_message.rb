eca63c663e7f2630f77c211e553318544d12a7dd
Andy Allan <andy@gravitystorm.co.uk>
2014-03-26 17:54:31 +0000

    Fix the creation of library item messages. Closes #182.

--- app/models/messages/library_item_message.rb
+++ app/models/messages/library_item_message.rb
@@ -12,5 +12,7 @@
 class LibraryItemMessage < MessageComponent
   belongs_to :item, class_name: 'Library::Item', foreign_key: 'library_item_id'
 
+  attr_accessible :library_item_id
+
   validates_presence_of :item
 end
