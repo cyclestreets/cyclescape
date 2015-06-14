32747a7496c8919dc377d8beb48232331dfc97eb
Nikolai Berkoff <alex.frost.dev@gmail.com>
2015-06-14 15:15:28 +0100

    rm unused code

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -19,9 +19,6 @@
 #
 
 class Vote < ActiveRecord::Base
-  scope :for_voter, -> { |*args| where(['voter_id = ? AND voter_type = ?', args.first.id, args.first.class.name]) }
-  scope :for_voteable, -> { |*args| where(['voteable_id = ? AND voteable_type = ?', args.first.id, args.first.class.name]) }
-  scope :recent, -> { |*args| where(['created_at > ?', (args.first || 2.weeks.ago)]) }
   scope :descending, -> { order('created_at DESC') }
 
   belongs_to :voteable, polymorphic: true
