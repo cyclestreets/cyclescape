1973b9f8fca2007992f4cfdad7a2be78addc639a
Nikolai Berkoff <nikolai-b@users.noreply.github.com>
2015-07-06 21:43:59 +0100

    add secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,16 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-secret_token = 'a1bcbfb276fb310924d6c5f8c7ca23d880200baf4bfe99a3769e2e889eedf6dd941d293f4d82fd38487da6fedbb71076d01ccf6f630ae64326a84d32ba2526ce'
+fallback_token = 'a1bcbfb276fb310924d6c5f8c7ca23d880200b'
 
 # check for the existence of a config/secret_token file, which we generate for produtions systems.
-secret_file = File.expand_path('../../secret_token', __FILE__)
-Cyclescape::Application.config.secret_token = File.exists?(secret_file) ? File.read(secret_file).strip : secret_token
+secret_file = Rails.root.join('config', 'secret_token')
+secret_key_base = Rails.root.join('config', 'secret_key_base')
+
+if Rails.env.production?
+  Cyclescape::Application.config.secret_token = File.read(secret_file).strip
+  Cyclescape::Application.config.secret_key_base = File.read(secret_key_base).strip
+else
+  Cyclescape::Application.config.secret_token = fallback_token
+  Cyclescape::Application.config.secret_key_base = fallback_token
+end
