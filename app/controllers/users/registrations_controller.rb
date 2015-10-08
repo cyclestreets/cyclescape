07a0bb984832ea560248a4771bde3798c4707ad2
Nikolai Berkoff <nikolai-b@users.noreply.github.com>
2015-10-08 08:14:56 +0100

    Fix redirect
    This should fix #416

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,22 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters
-
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update).push(:full_name, :display_name)
-    devise_parameter_sanitizer.for(:sign_up).push(:full_name, :display_name)
-  end
-
   def after_inactive_sign_up_path_for(resource)
-    root_url
+    root_url(protocol: 'http')
   end
 
   def after_sign_up_path_for(resource)
-    root_url
+    root_url(protocol: 'http')
   end
 
   def after_update_path_for(resource)
-    edit_user_registration_url
+    edit_user_registration_url(protocol: 'http')
   end
 end
