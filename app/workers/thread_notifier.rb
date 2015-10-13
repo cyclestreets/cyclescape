f7d8998884acb54a0731cdeacc6d43a15b8339e1
Nikolai Berkoff <nikolai-b@users.noreply.github.com>
2015-10-13 22:39:57 +0100

    Simplify thread notifications

--- app/workers/thread_notifier.rb
+++ app/workers/thread_notifier.rb
@@ -8,24 +8,11 @@ class ThreadNotifier
     send(method, *args)
   end
 
-  # Main entry point, begin notification of subscribers to +thread+ with message +type+ and
+  # Notification of subscribers to +thread+ with message +type+ and
   # optional +message+ object
   def self.notify_subscribers(thread, type, message = nil)
-    Resque.enqueue(ThreadNotifier, :queue_messages_for_subscribers, thread.id, type, message ? message.id : nil)
-  end
-
-  # Queue the notification for each subscriber
-  def self.queue_messages_for_subscribers(thread_id, type, message_id)
-    thread = MessageThread.find(thread_id)
     thread.email_subscribers.each do |subscriber|
-      Resque.enqueue(ThreadNotifier, :send_notification, type, message_id, subscriber.id)
+      ThreadMailer.send(type, message, subscriber).deliver_later
     end
-  end
-
-  # Generate and send out the notification email
-  def self.send_notification(type, message_id, subscriber_id)
-    message = Message.find(message_id)
-    subscriber = message.thread.subscribers.find(subscriber_id)
-    ThreadMailer.send(type, message, subscriber).deliver_later
   end
 end
