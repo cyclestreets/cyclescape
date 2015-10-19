ce5f355ed253d50c033e619a6f754699848fe34c
mvl22 <martin@lucas-smith.co.uk>
2015-10-19 13:01:05 +0100

    Add Resque logging

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,4 @@
 Resque.redis.namespace = 'resque:Cyclescape'
 Resque.inline = true if Rails.env.test?
 Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
+Resque.logger = Logger.new("#{Rails.root}/log/resque.log")
