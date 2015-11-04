061490bbbefc21e866c80f32be4af7e6b629ea33
Nikolai B <nikolai-b@users.noreply.github.com>
2015-11-04 09:47:45 +0000

    Hide spam threads on issue show

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -18,7 +18,7 @@ class IssuesController < ApplicationController
     set_page_title @issue.title
     set_page_description @issue.description
     set_page_image @issue.photo.try(:url)
-    @threads = ThreadListDecorator.decorate_collection @issue.threads.order_by_latest_message.includes(:group)
+    @threads = ThreadListDecorator.decorate_collection @issue.threads.approved.order_by_latest_message.includes(:group)
     @tag_panel = TagPanelDecorator.new(@issue, form_url: issue_tags_path(@issue), cancel_url: issue_path(@issue))
   end
 
