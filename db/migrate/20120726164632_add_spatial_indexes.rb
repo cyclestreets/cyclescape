35780290bf5af66afee5dd82a463af71267b4ffb
Nikolai B <nikolai-b@users.noreply.github.com>
2015-11-04 15:56:47 +0000

    More old migration fixes

--- db/migrate/20120726164632_add_spatial_indexes.rb
+++ db/migrate/20120726164632_add_spatial_indexes.rb
@@ -1,8 +1,8 @@
 class AddSpatialIndexes < ActiveRecord::Migration
   def change
-    add_index(:issues, :location, spatial: true)
-    add_index(:group_profiles, :location, spatial: true)
-    add_index(:library_items, :location, spatial: true)
-    add_index(:user_locations, :location, spatial: true)
+    add_index(:issues, :location, using: :gist)
+    add_index(:group_profiles, :location, using: :gist)
+    add_index(:library_items, :location, using: :gist)
+    add_index(:user_locations, :location, using: :gist)
   end
 end
