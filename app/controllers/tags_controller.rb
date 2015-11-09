2f0cfdf1b64b957168042eb2b6faa0ee29328d13
Nikolai B <nikolai-b@users.noreply.github.com>
2015-11-09 14:10:29 +0000

    Harmonize tag results with other search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,11 +4,12 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find_by_name params[:id]
     if @tag
-      @issues = Issue.find_by_tag(@tag).order('updated_at desc').limit(20)
+      issues = Issue.find_by_tag(@tag).order('updated_at desc').page(params[:issue_page])
+      @issues = IssueDecorator.decorate_collection issues
       # Threads - argh, privacy!
-      threads = MessageThread.is_public.find_by_tag(@tag).order('updated_at desc').limit(20)
+      threads = MessageThread.is_public.find_by_tag(@tag).order('updated_at desc').page(params[:thread_page])
       @threads = ThreadListDecorator.decorate_collection threads
-      @library_items = Library::Item.find_by_tag(@tag).order('updated_at desc').limit(20)
+      @library_items = Library::Item.find_by_tag(@tag).order('updated_at desc').page(params[:library_page])
     else
       @unrecognised_tag_name = params[:id]
     end
