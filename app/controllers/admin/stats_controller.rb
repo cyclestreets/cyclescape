fd2927df907c86b103e01ae97ab9b059ce1ec2ab
Nikolai B <nikolai-b@users.noreply.github.com>
2015-12-07 22:09:18 +0000

    Add planning app stats

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -6,8 +6,9 @@ class Admin::StatsController < ApplicationController
                                 date_trunc('month', created_at) AS month,
                                 COUNT(DISTINCT created_by_id) AS cids").group('month')
 
-    @issues = Issue.select("COUNT(*) AS count,
-                           date_trunc('month', created_at) AS month").group(:month)
+    @issues = Issue.joins("LEFT OUTER JOIN planning_applications ON planning_applications.issue_id = issues.id").
+      select("COUNT(issues.*) AS count, date_trunc('month', issues.created_at) AS month,
+    COUNT(planning_applications.*) AS pa").group(:month)
 
     @message_types = [DeadlineMessage, LinkMessage, PhotoMessage, StreetViewMessage].
       each_with_object({}) do |klass, hsh|
