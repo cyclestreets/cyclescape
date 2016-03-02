36c214fb6f53a2db85cda19f399a7f14a4c1d274
Nikolai B <nikolai-b@users.noreply.github.com>
2016-03-02 08:21:16 +0000

    Use SSL tile server

--- config/initializers/map_layers.rb
+++ config/initializers/map_layers.rb
@@ -1,17 +1,17 @@
 module MapLayers
   OPENCYCLEMAP = OpenLayers::Layer::OSM.new(
     'OpenCycleMap',
-    %w(a b c).map { |k| "http://#{k}.tile.cyclestreets.net/opencyclemap/${z}/${x}/${y}@2x.png" },
+    'https://tile.cyclestreets.net/opencyclemap/${z}/${x}/${y}@2x.png',
     opacity: 0.8, tileOptions: { crossOriginKeyword: :null }
   )
   OS_STREETVIEW = OpenLayers::Layer::OSM.new(
     'OS StreetView',
-    %w(a b c).map { |k| "http://#{k}.tile.cyclestreets.net/osopendata/${z}/${x}/${y}.png" },
+    'https://tile.cyclestreets.net/osopendata/${z}/${x}/${y}.png',
     opacity: 0.8, tileOptions: { crossOriginKeyword: :null }
   )
   MAPNIK = OpenLayers::Layer::OSM.new(
     'OpenStreetMap',
-    %w(a b c).map { |k| "http://#{k}.tile.cyclestreets.net/mapnik/${z}/${x}/${y}.png" },
+    'https://tile.cyclestreets.net/mapnik/${z}/${x}/${y}.png',
     opacity: 0.8, tileOptions: { crossOriginKeyword: :null }
   )
 end
