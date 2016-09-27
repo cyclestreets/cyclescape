e4a4feb70a7a3b00dfff5ef7a7dbe0b4263e7d0f
Nikolai B <nikolai-b@users.noreply.github.com>
2016-09-27 23:21:29 +0100

    Fix specs

--- spec/models/messages/cyclestreets_photo_message_spec.rb
+++ spec/models/messages/cyclestreets_photo_message_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require 'spec_helper'
 
-describe CyclestreetsPhoto, type: :model do
+describe CyclestreetsPhotoMessage, type: :model do
   it { is_expected.to validate_presence_of(:photo) }
   it { is_expected.to validate_presence_of(:location) }
 end
