11c70eadde531252c63098c2574c0fce95cb273f
Nikolai B <nikolai-b@users.noreply.github.com>
2016-10-24 23:30:48 +0100

    Fix strangeness with test suite
    Creating a group with no profile auto
    creates a profile.  When using the
    group_profile factory this meant
    two profiles were created for the one group.
    Changing to build means the group is not
    created until the profile is build (so no spurious
    auto created profile is made)

--- spec/factories/group_profiles.rb
+++ spec/factories/group_profiles.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     description 'This is a group of people who want to do some stuff. It probably involves cycling, but you can never be certain about that'
     joining_instructions 'Jump up get down stand up turn around. Twice. While cycling backwards'
     location 'POINT(-122 47)'
-    association :group, factory: :group
     new_user_email 'Hi'
+    association :group, factory: :group, strategy: :build
 
     trait :with_picture do
       picture { Pathname.new(File.join(%w(spec support images abstract-100-100.jpg))) }
@@ -19,7 +19,7 @@ FactoryGirl.define do
     end
 
     factory :quahogcc_group_profile do
-      association :group, factory: :quahogcc
+      association :group, factory: :quahogcc, strategy: :build
       location 'POLYGON ((-0.048337826538099 52.288247679276, -0.095029721069341 52.334425101058, -0.0064524505615302 52.370493901594, 0.084528079223625 52.301266191436, 0.26065265197753 52.294127481182, 0.26751910705777 52.191539167527, 0.22906695861468 52.15279673993, 0.18100177306972 52.101368865428, 0.12469684143375 52.097994471963, 0.073885073849465 52.148583573398, -0.13622845154113 52.2142635765, -0.048337826538099 52.288247679276))'
     end
   end
