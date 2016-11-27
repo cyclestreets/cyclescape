c9fa3cbab991180f700e7112516fe5c1b0a8e2df
Nikolai B <nikolai-b@users.noreply.github.com>
2016-11-27 19:02:32 +0000

    Always subscribe creator of message to thread
    Fixes #659

--- app/models/message.rb
+++ app/models/message.rb
@@ -119,7 +119,7 @@ class Message < ActiveRecord::Base
   end
 
   def approve_related
-    thread.add_subscriber(created_by) unless created_by.ever_subscribed_to_thread?(thread)
+    thread.add_subscriber(created_by) unless created_by.subscribed_to_thread?(thread)
     created_by.approve! unless created_by.approved?
     if thread.approved?
       ThreadNotifier.notify_subscribers thread, self
