688b9d71e3974d247b93610c7fc904e4366ac8ed
Nikolai B <nikolai-b@users.noreply.github.com>
2017-02-11 23:05:06 +0000

    Set HOME for bundler CRON jobs
    Without this get warning Your home directory is not set properly:

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,5 @@
 env :PATH, ENV['PATH']
+env :HOME, "/var/www/cyclescape/shared/bundle"
 job_type :runner,  "cd :path && /usr/local/bin/bundle exec rails runner -e :environment ':task' :output"
 
 # Use this file to easily define all of your cron jobs.
