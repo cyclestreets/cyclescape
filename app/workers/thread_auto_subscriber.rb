6d28a33e2d264113a4e8bfdb80f3e1e77f49ed15
Nikolai B <nikolai-b@users.noreply.github.com>
2017-04-21 22:34:14 +0100

    Lock the thread during subscribe
    Refs #711

--- app/workers/thread_auto_subscriber.rb
+++ app/workers/thread_auto_subscriber.rb
@@ -6,42 +6,44 @@ class ThreadAutoSubscriber
 
     def perform(thread_id, changes)
       thread = MessageThread.find(thread_id)
-      if changes.keys.include?("privacy")
-        case thread.privacy
-        when 'committee'
-          thread.subscribers.each do |subscriber|
-            unless thread.group.committee_members.include?(subscriber)
-              subscriber.thread_subscriptions.to(thread).destroy
-              # TODO add removal notification
-            end
-          end
-        when 'group'
-          if changes["privacy"].first == 'public'
+      thread.with_lock do
+        if changes.keys.include?("privacy")
+          case thread.privacy
+          when 'committee'
             thread.subscribers.each do |subscriber|
-              unless thread.group.members.include?(subscriber)
+              unless thread.group.committee_members.include?(subscriber)
                 subscriber.thread_subscriptions.to(thread).destroy
                 # TODO add removal notification
               end
             end
-          elsif changes["privacy"].first == 'committee'
+          when 'group'
+            if changes["privacy"].first == 'public'
+              thread.subscribers.each do |subscriber|
+                unless thread.group.members.include?(subscriber)
+                  subscriber.thread_subscriptions.to(thread).destroy
+                  # TODO add removal notification
+                end
+              end
+            elsif changes["privacy"].first == 'committee'
+              ThreadSubscriber.subscribe_users(thread)
+              # TODO add available notification for involvegroups / locations :notify
+            end
+          when 'public'
             ThreadSubscriber.subscribe_users(thread)
-            # TODO add available notification for involvegroups / locations :notify
+            # TODO add notification for involvegroups / locations :notify
           end
-        when 'public'
-          ThreadSubscriber.subscribe_users(thread)
-          # TODO add notification for involvegroups / locations :notify
         end
-      end
 
-      if changes.keys.include?("issue_id")
-        if thread.issue
-          ThreadSubscriber.subscribe_users(thread)
-          # TODO notifications
-        else
-          thread.subscribers.each do |subscriber|
-            unless thread.participants.include?(subscriber) || (thread.group && thread.group.members.include?(subscriber) && subscriber.prefs.involve_my_groups_admin)
-              subscriber.thread_subscriptions.to(thread).destroy
-              # TODO add removal notification
+        if changes.keys.include?("issue_id")
+          if thread.issue
+            ThreadSubscriber.subscribe_users(thread)
+            # TODO notifications
+          else
+            thread.subscribers.each do |subscriber|
+              unless thread.participants.include?(subscriber) || (thread.group && thread.group.members.include?(subscriber) && subscriber.prefs.involve_my_groups_admin)
+                subscriber.thread_subscriptions.to(thread).destroy
+                # TODO add removal notification
+              end
             end
           end
         end
