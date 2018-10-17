c7c9e7d74e8b59262c7f7a4a9c456a42f7906a6e
Nikolai B <nikolai-b@users.noreply.github.com>
2018-10-17 21:54:18 +0100

    Add google_street_view_api_key to site config
    Fixes #801

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181014184337) do
+ActiveRecord::Schema.define(version: 20181017203315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -479,7 +479,7 @@ ActiveRecord::Schema.define(version: 20181014184337) do
 
   create_table "site_configs", force: :cascade do |t|
     t.string   "logo_uid"
-    t.string   "application_name",                                                                                                                                null: false
+    t.string   "application_name",                                                                                                                                  null: false
     t.string   "funder_image_footer1_uid"
     t.string   "funder_image_footer2_uid"
     t.string   "funder_image_footer3_uid"
@@ -498,36 +498,37 @@ ActiveRecord::Schema.define(version: 20181014184337) do
     t.string   "funder_url_footer4"
     t.string   "funder_url_footer5"
     t.string   "funder_url_footer6"
-    t.geometry "nowhere_location",         limit: {:srid=>4326, :type=>"geometry"},                                                                               null: false
+    t.geometry "nowhere_location",           limit: {:srid=>4326, :type=>"geometry"},                                                                               null: false
     t.string   "facebook_link"
     t.string   "twitter_link"
-    t.string   "default_locale",                                                                                                                                  null: false
-    t.string   "timezone",                                                                                                                                        null: false
+    t.string   "default_locale",                                                                                                                                    null: false
+    t.string   "timezone",                                                                                                                                          null: false
     t.string   "ga_account_id"
     t.string   "ga_base_domain"
-    t.string   "default_email",                                                                                                                                   null: false
-    t.string   "email_domain",                                                                                                                                    null: false
-    t.string   "geocoder_url",                                                                                                                                    null: false
+    t.string   "default_email",                                                                                                                                     null: false
+    t.string   "email_domain",                                                                                                                                      null: false
+    t.string   "geocoder_url",                                                                                                                                      null: false
     t.string   "geocoder_key"
-    t.datetime "created_at",                                                                                                                                      null: false
-    t.datetime "updated_at",                                                                                                                                      null: false
-    t.string   "admin_email",                                                       default: "cyclescape-comments@cyclestreets.net",                              null: false
-    t.string   "blog_url",                                                          default: "http://blog.cyclescape.org/",                                       null: false
-    t.string   "blog_user_guide_url",                                               default: "http://blog.cyclescape.org/guide/",                                 null: false
-    t.string   "tile_server1_name",                                                 default: "OpenCycleMap",                                                      null: false
-    t.string   "tile_server1_url",                                                  default: "https://{s}.tile.cyclestreets.net/opencyclemap/{z}/{x}/{y}@2x.png", null: false
-    t.string   "tile_server2_name",                                                 default: "OS StreetView"
-    t.string   "tile_server2_url",                                                  default: "https://{s}.tile.cyclestreets.net/osopendata/{z}/{x}/{y}.png"
-    t.string   "tile_server3_name",                                                 default: "OpenStreetMap"
-    t.string   "tile_server3_url",                                                  default: "https://{s}.tile.cyclestreets.net/mapnik/{z}/{x}/{y}.png"
-    t.string   "blog_about_url",                                                    default: "http://blog.cyclescape.org/about/",                                 null: false
+    t.datetime "created_at",                                                                                                                                        null: false
+    t.datetime "updated_at",                                                                                                                                        null: false
+    t.string   "admin_email",                                                         default: "cyclescape-comments@cyclestreets.net",                              null: false
+    t.string   "blog_url",                                                            default: "http://blog.cyclescape.org/",                                       null: false
+    t.string   "blog_user_guide_url",                                                 default: "http://blog.cyclescape.org/guide/",                                 null: false
+    t.string   "tile_server1_name",                                                   default: "OpenCycleMap",                                                      null: false
+    t.string   "tile_server1_url",                                                    default: "https://{s}.tile.cyclestreets.net/opencyclemap/{z}/{x}/{y}@2x.png", null: false
+    t.string   "tile_server2_name",                                                   default: "OS StreetView"
+    t.string   "tile_server2_url",                                                    default: "https://{s}.tile.cyclestreets.net/osopendata/{z}/{x}/{y}.png"
+    t.string   "tile_server3_name",                                                   default: "OpenStreetMap"
+    t.string   "tile_server3_url",                                                    default: "https://{s}.tile.cyclestreets.net/mapnik/{z}/{x}/{y}.png"
+    t.string   "blog_about_url",                                                      default: "http://blog.cyclescape.org/about/",                                 null: false
     t.string   "small_logo_uid"
-    t.string   "tile_server1_type",                                                 default: "layers",                                                            null: false
-    t.string   "tile_server2_type",                                                 default: "layers",                                                            null: false
-    t.string   "tile_server3_type",                                                 default: "layers",                                                            null: false
-    t.string   "tile_server1_options",                                              default: "{}",                                                                null: false
-    t.string   "tile_server2_options",                                              default: "{}",                                                                null: false
-    t.string   "tile_server3_options",                                              default: "{}",                                                                null: false
+    t.string   "tile_server1_type",                                                   default: "layers",                                                            null: false
+    t.string   "tile_server2_type",                                                   default: "layers",                                                            null: false
+    t.string   "tile_server3_type",                                                   default: "layers",                                                            null: false
+    t.string   "tile_server1_options",                                                default: "{}",                                                                null: false
+    t.string   "tile_server2_options",                                                default: "{}",                                                                null: false
+    t.string   "tile_server3_options",                                                default: "{}",                                                                null: false
+    t.string   "google_street_view_api_key"
   end
 
   create_table "street_view_messages", force: :cascade do |t|
