8880fe0271b551524937b8c78b70be7bf2eaf9e9
Nikolai B <nikolai-b@users.noreply.github.com>
2019-03-11 22:20:40 +0000

    Fix spec
    With after_commit and reworking issue and thread into one form

--- spec/features/issues_with_notifications_spec.rb
+++ spec/features/issues_with_notifications_spec.rb
@@ -1,7 +1,7 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'Issue notifications' do
-  include_context 'signed in as a site user'
+describe "Issue notifications" do
+  include_context "signed in as a site user"
 
   let(:issue_values) { attributes_for(:issue_with_json_loc, description: "hump & martini") }
   let(:thread_values) { attributes_for(:message_thread) }
@@ -11,30 +11,30 @@ describe 'Issue notifications' do
     visit new_issue_path if page.current_path != new_issue_path
     fill_in I18n.t("formtastic.labels.issue.new.title"), with: issue_values[:title]
     fill_in I18n.t("formtastic.labels.issue.new.description"), with: issue_values[:description]
-    fill_in I18n.t("formtastic.labels.issue.new.tags_string"), with: 'parking parking' # should cope with duplicates
-    find('#issue_loc_json', visible: false).set(location)
+    fill_in I18n.t("formtastic.labels.issue.new.tags_string"), with: "parking parking" # should cope with duplicates
+    find("#issue_loc_json", visible: false).set(location)
     fill_in I18n.t("activerecord.attributes.message_thread.title"), with: thread_values[:title]
     fill_in "Message", with: "New message text"
   end
 
-  context 'on a new issue' do
-    it 'should create a new issue' do
+  context "on a new issue" do
+    it "should create a new issue" do
       visit new_issue_path
 
-      maxlength = find_field('Title')['maxlength']
+      maxlength = find_field("Title")["maxlength"]
       expect(maxlength).to eq("80")
 
       fill_in_issue
       click_on I18n.t("formtastic.actions.issue.create")
 
-      within('#content header') do
+      within("#content header") do
         expect(page).to have_content(issue_values[:title])
       end
-      expect(page).to have_content('parking')
+      expect(page).to have_content("parking")
       expect(page).to have_content(current_user.name)
     end
 
-    describe 'for users with overlapping user locations' do
+    describe "for users with overlapping user locations" do
       let(:user) { create(:user) }
       let!(:user_location) { create(:user_location, user: user, loc_json: issue_values[:loc_json]) }
       let(:email) { open_last_email_for(user.email) }
@@ -51,12 +51,13 @@ describe 'Issue notifications' do
         context "when the user wants to be notified" do
           let(:location_email_prefs) { "notify" }
 
-          it 'should send a notification' do
+          it "should send a notification" do
             expect(page).to have_content(issue_values[:title])
+            email = all_emails.find { |e| e.to == [user.email] && e.subject.start_with?("[Cyclescape] New issue -") }
             expect(email).to have_subject("[Cyclescape] New issue - \"#{issue_values[:title]}\"")
             expect(email).to have_body_text(issue_values[:title])
             expect(email).to have_body_text(issue_values[:description])
-            expect(email).not_to have_body_text('&amp;')
+            expect(email).not_to have_body_text("&amp;")
             expect(email).to have_body_text(current_user.name)
           end
         end
@@ -64,7 +65,7 @@ describe 'Issue notifications' do
         context "when the user wants to be subscribed" do
           let(:location_email_prefs) { "subscribe" }
 
-          it 'should send an email' do
+          it "should send an email" do
             expect(email).not_to be_nil
           end
         end
@@ -80,7 +81,7 @@ describe 'Issue notifications' do
       end
     end
 
-    describe 'for users in groups with overlapping locations' do
+    describe "for users in groups with overlapping locations" do
       let!(:group_profile) { create(:quahogcc_group_profile) }
       let!(:notifiee) { create(:user) }
       let!(:group_membership) { create(:group_membership, user: notifiee, group: group_profile.group) }
@@ -95,8 +96,10 @@ describe 'Issue notifications' do
       context "when the user wants notifications" do
         let(:group_email_prefs) { "notify" }
 
-        it 'should send a notification' do
-          expect(email).to have_subject("[Cyclescape] New issue - \"#{issue_values[:title]}\" (#{group_profile.group.name})")
+        it "should send a notification" do
+          expect(email).to have_subject(
+            "[Cyclescape] New issue - \"#{issue_values[:title]}\" (#{group_profile.group.name})"
+          )
           email_body = email.text_part.decoded
           expect(email_body).to include("in the #{group_profile.group.name}'s area")
           expect(email_body).to include(issue_values[:description])
@@ -114,40 +117,52 @@ describe 'Issue notifications' do
     end
   end
 
-  context 'multiple overlapping locations' do
+  context "multiple overlapping locations" do
     let(:location_category) { create :location_category }
     let(:user) { create(:user) }
     let(:user2) { create(:user) }
-    let!(:user_location) { create(:user_location, user: user, location: 'POLYGON ((0.1 0.1, 0.1 0.2, 0.2 0.2, 0.2 0.1, 0.1 0.1))') }
+    let!(:user_location) do
+      create(:user_location, user: user, location: "POLYGON ((0.1 0.1, 0.1 0.2, 0.2 0.2, 0.2 0.1, 0.1 0.1))")
+    end
     let(:location) { user_location.loc_json }
 
     before do
-      user.prefs.update!(involve_my_locations: 'notify', email_status_id: 1)
-      user2.prefs.update!(involve_my_locations: 'notify', email_status_id: 1)
+      user.prefs.update!(involve_my_locations: "notify", email_status_id: 1)
+      user2.prefs.update!(involve_my_locations: "notify", email_status_id: 1)
       fill_in_issue
     end
 
-    it 'should not send multiple emails to the same user' do
+    it "should not send multiple emails to the same user" do
       email_count = all_emails.count
       click_on I18n.t("formtastic.actions.issue.create")
-      expect(all_emails.count).to eq(email_count + 1)
+      expect(all_emails.count).to eq(email_count + 2)
+      expect(all_emails.map(&:subject)).to match_array(
+        [/\[Cyclescape\] New issue/, /\[Cyclescape\] New thread started on issue/]
+      )
     end
 
-    context 'multiple users' do
+    context "multiple users" do
       let!(:user2_location) do
-        create(:user_location, user: user2, location: 'POLYGON ((0.1 0.1, 0.1 0.2, 0.2 0.2, 0.2 0.1, 0.1 0.1))')
+        create(:user_location, user: user2, location: "POLYGON ((0.1 0.1, 0.1 0.2, 0.2 0.2, 0.2 0.1, 0.1 0.1))")
       end
 
-      it 'should send one email to multiple users' do
+      it "should send one email to multiple users" do
         email_count = all_emails.count
         click_on I18n.t("formtastic.actions.issue.create")
-        expect(all_emails.count).to eq(email_count + 2)
+        expect(all_emails.count).to eq(email_count + 4)
+        emails_by_to = all_emails.group_by(&:to)
+        emails_by_to.values do |emails|
+          expect(emails.map(&:subject)).to match_array(
+            [/\[Cyclescape\] New issue/, /\[Cyclescape\] New thread started on issue/]
+          )
+        end
+        expect(emails_by_to.keys.flatten).to match_array [user.email, user2.email]
       end
     end
   end
 
-  context 'overlapping group and user locations' do
-    let(:location_string) { 'POLYGON ((0.1 0.1, 0.1 0.2, 0.2 0.2, 0.2 0.1, 0.1 0.1))' }
+  context "overlapping group and user locations" do
+    let(:location_string) { "POLYGON ((0.1 0.1, 0.1 0.2, 0.2 0.2, 0.2 0.1, 0.1 0.1))" }
     let(:location) { user_location.loc_json }
     let(:user) { create(:user) }
     let!(:user_location) { create(:user_location, user: user, location: location_string) }
@@ -156,19 +171,22 @@ describe 'Issue notifications' do
 
     before do
       user.prefs.update!(
-        involve_my_locations: 'notify',
-        involve_my_groups: 'notify',
+        involve_my_locations: "notify",
+        involve_my_groups: "notify",
         email_status_id: 1
       )
       fill_in_issue
     end
 
-    # The user would normally receive two emails - one for the issue being within the group's area,
+    # The user would normally receive two emails - one for the issue being within the group"s area,
     # and a second email since the issue is also in one of their user locations.
-    it 'should only send one email to the user' do
+    it "should only send one email to the user" do
       email_count = all_emails.count
       click_on I18n.t("formtastic.actions.issue.create")
-      expect(all_emails.count).to eq(email_count + 1)
+      expect(all_emails.count).to eq(email_count + 2)
+      expect(all_emails.map(&:subject)).to match_array(
+        [/\[Cyclescape\] New issue/, /\[Cyclescape\] New thread started on issue/]
+      )
     end
   end
 end
