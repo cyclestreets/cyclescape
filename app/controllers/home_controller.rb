b2f46b58317cdcf0a9699f6d833f85346cfd0e4e
Nikolai B <nikolai-b@users.noreply.github.com>
2019-06-10 09:42:04 +0100

    Preload issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   filter_access_to :all, require: :show
 
   def show
-    latest_threads = ThreadList.recent_public.limit(6).includes(messages: :created_by).to_a
+    latest_threads = ThreadList.recent_public.limit(6).includes(messages: :created_by, issue: :tags).to_a
     @latest_threads = ThreadListDecorator.decorate_collection(latest_threads)
     @unviewed_thread_ids = MessageThread.unviewed_thread_ids(user: current_user, threads: latest_threads)
     if current_user
