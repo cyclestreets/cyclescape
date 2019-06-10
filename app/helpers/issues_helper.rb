4601d20eb9dc86809467293509992bcb73f2df58
Nikolai B <nikolai-b@users.noreply.github.com>
2019-06-10 09:40:23 +0100

    Cache many of the views

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -2,7 +2,7 @@
 
 module IssuesHelper
   def issues_list(issues, options = {})
-    defaults = { partial: "issues/compact", collection: issues, as: :issue, locals: { prefix: "list" } }
+    defaults = { partial: "issues/compact", collection: issues, as: :issue, locals: { prefix: "list" }, cached: true }
     render defaults.merge(options)
   end
 end
