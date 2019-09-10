ce758aca25fd1757c3158627c8759f3e2d6ff3e7
Nikolai B <nikolai-b@users.noreply.github.com>
2019-09-10 21:57:16 +0100

    Fix site stats

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = "1.0"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif ie7.css street_view.js]
+Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif ie7.css street_view.js chartkick.js]
