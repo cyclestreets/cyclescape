37dfab6172c0524af60e50d52023906e28666824
Nikolai B <nikolai-b@users.noreply.github.com>
2019-10-05 20:51:50 +0100

    Remove and trim message component controllers

--- app/controllers/message/cyclestreets_photos_controller.rb
+++ app/controllers/message/cyclestreets_photos_controller.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 
-class Message::CyclestreetsPhotosController < Message::BaseController
+class Message::CyclestreetsPhotosController < ApplicationController
   def show
-    @cyclestreets_photo = resource_class.find params[:id]
-  end
-
-  protected
-
-  def resource_class
-    CyclestreetsPhotoMessage
-  end
-
-  def permit_params
-    %i[photo_url caption cyclestreets_id icon_properties loc_json]
+    @cyclestreets_photo = CyclestreetsPhotoMessage.find params[:id]
   end
 end
