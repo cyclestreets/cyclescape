0de43d6dc87f4a29a45d9c3fd43421c3604124ba
Nikolai B <nikolai-b@users.noreply.github.com>
2020-01-16 20:32:16 +0000

    Remove site comment modal
    Refs #901

--- app/controllers/site/comments_controller.rb
+++ app/controllers/site/comments_controller.rb
@@ -6,18 +6,21 @@ class Site::CommentsController < ApplicationController
   end
 
   def new
+    session[:site_comment_referer] = request.referer
     @site_comment = current_user ? current_user.site_comments.new : SiteComment.new
   end
 
   def create
     @site_comment = SiteComment.new permitted_params
 
-    @site_comment.user = current_user if current_user
+    @site_comment.user = current_user
+    @site_comment.context_url = session[:site_comment_referer]
 
     if params[:bicycle_wheels].try(:strip) == "8" && params[:real_name].blank? && @site_comment.save
       set_flash_message(:success)
       AdminMailer.new_site_comment(@site_comment).deliver_later
-      redirect_to request.referer || root_path
+      redirect_to session[:site_comment_referer] || root_path
+      session[:site_comment_referer] = nil
     else
       render "new", status: :conflict
     end
@@ -36,6 +39,6 @@ class Site::CommentsController < ApplicationController
   protected
 
   def permitted_params
-    params.require(:site_comment).permit :name, :email, :body, :context_url, :context_data
+    params.require(:site_comment).permit :name, :email, :body, :context_url
   end
 end
