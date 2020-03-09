7d3c9d5b450430c85e14e8e1f0e70c56e0970bc4
Nikolai B <nikolai-b@users.noreply.github.com>
2020-03-09 23:26:38 +0000

    Try to fix facebook login
    Re #200

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -242,4 +242,16 @@ Devise.setup do |config|
   # Added during Devise upgrade
   config.password_length = 6..128
   config.reconfirmable = false
+
+  # Errors from authentication may crash because no Devise mapping is defined
+  # for the callback path. To work around this we manually set the failure
+  # callback to Users::OmniauthCallbacksController.failure().
+  #
+  # See https://github.com/plataformatec/devise/issues/2004#issuecomment-7466322# for more information.
+  OmniAuth.config.on_failure = proc do |env|
+    env["devise.mapping"] = Devise.mappings[:user]
+    controller_name = ActiveSupport::Inflector.camelize(env["devise.mapping"].controllers[:omniauth_callbacks])
+    controller_klass = ActiveSupport::Inflector.constantize("#{controller_name}Controller")
+    controller_klass.action(:failure).call(env)
+  end
 end
