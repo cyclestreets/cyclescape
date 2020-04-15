fd10402f503e91b4c6f7e50b5002d36be564f659
Nikolai B <nikolai-b@users.noreply.github.com>
2020-04-15 22:53:26 +0100

    Update rakismet URL

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 akismet_file = Rails.root.join("config", "akismet")
-if akismet_file.exist?
-  Cyclescape::Application.config.rakismet.key = akismet_file.read.strip
-  Cyclescape::Application.config.rakismet.url = "http://www.cyclescape.org/"
-elsif %w[development test].include? Rails.env
-  Cyclescape::Application.config.rakismet.key = "development"
-  Cyclescape::Application.config.rakismet.url = "http://www.cyclescape.org/"
-end
+
+Cyclescape::Application.config.rakismet.key =
+  if akismet_file.exist?
+    akismet_file.read.strip
+  elsif %w[development test].include? Rails.env
+    "development"
+  end
+
+Cyclescape::Application.config.rakismet.url = "https://www.cyclescape.org/"
