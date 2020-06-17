ccf45c2d6ee9e0fe9a4e0ba833b32f3c0b8b2c4a
Nikolai B <nikolai-b@users.noreply.github.com>
2020-06-17 22:47:19 +0100

    Add deadline timezone spec

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -12,4 +12,23 @@ describe Notifications do
     subject = described_class.send(:added_to_group, gm)
     expect(subject.body).to include("big hello to #{user.full_name} nothing to  here")
   end
+
+  describe "thread deadlines" do
+    let(:deadline) { create :deadline_message, deadline: Time.utc(2020, 6, 1, 10) } # 10am UTC is 11am BST
+
+    it "sets the correct timezone" do
+      subject = described_class.upcoming_thread_deadline(deadline.created_by, deadline.message.thread, deadline)
+      expect(subject.body).to include("June 1st, 2020 11:00")
+    end
+  end
+
+  describe "issue deadlines" do
+    let(:issue)  { create :issue, deadline: Time.utc(2020, 6, 1, 10) } # 10am UTC is 11am BST
+    let(:thread) { create :message_thread, issue: issue }
+
+    it "sets the correct timezone" do
+      subject = described_class.upcoming_issue_deadline(issue.created_by, issue, thread)
+      expect(subject.body).to include("June 1st, 2020 11:00")
+    end
+  end
 end
