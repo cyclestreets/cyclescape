7fda8a65c2530a632334286982ace3e9b87847e8
Nikolai B <nikolai-b@users.noreply.github.com>
2022-03-05 21:23:24 +0000

    Replace dummy threads in Threads#index
    Remove the dummy popular, mine etc. from the tabs

--- app/controllers/new_ui/threads_controller.rb
+++ app/controllers/new_ui/threads_controller.rb
@@ -3,30 +3,29 @@
 module NewUi
   class ThreadsController < BaseController
     def index
-      threads =
-        if current_user
-          @current_group = current_user.groups.first
-          ThreadList.recent_from_groups(current_user.groups)
-        else
-          ThreadList.recent_public
-        end
-
-      @all = threads.where.not(issue_id: nil).page(1)
-      @popular =
-        if @current_group
-          @current_group.threads.ordered_by_nos_of_messages.page(1)
-        else
-          MessageThread.ordered_by_nos_of_messages.page(1)
-        end
-      @mine = threads.where(created_by: current_user).page(params[:my_issues_page])
-      @favourite = current_user.favourite_threads.page(1)
-
-      @unviewed_message_count = {}
       if current_user
         @current_group = current_user.groups.first
-        MessageThread.where(id: @all.map(&:id) + @popular.map(&:id) + @mine.map(&:id) + @favourite.map(&:id)).unviewed_message_counts(current_user).each_with_object(@unviewed_message_count) do |(issue_id, count), hsh|
+        threads = ThreadList.recent_from_groups(current_user.groups)
+        @all = threads.where.not(issue_id: nil).page(1)
+        @popular = @current_group.threads.where.not(issue_id: nil).ordered_by_nos_of_messages.page(1)
+        @mine = threads.where(created_by: current_user).page(1)
+        @favourite = current_user.favourite_threads.page(1)
+
+        @unviewed_message_count = {}
+        @current_group = current_user.groups.first
+        ids = @all.map(&:id) + @popular.map(&:id) + @mine.map(&:id) + @favourite.map(&:id)
+        MessageThread
+          .where(id: ids).unviewed_message_counts(current_user)
+          .each_with_object(@unviewed_message_count) do |(issue_id, count), hsh|
           hsh[issue_id] = count
         end
+      else
+        threads = ThreadList.recent_public
+        @all = threads.where.not(issue_id: nil).page(1)
+        @popular = MessageThread.none
+        @mine = MessageThread.none
+        @favourite = MessageThread.none
+        @unviewed_message_counts = {}
       end
     end
   end
