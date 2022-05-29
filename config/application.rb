1c71f73764952c95013d75d955db9ce6f4c86d8a
Nikolai B <nikolai-b@users.noreply.github.com>
2022-05-29 20:19:14 +0100

    Allow rails i18n fallbacks

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module Cyclescape
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = "en-GB"
+    config.i18n.fallbacks = [I18n.default_locale, { "en-GB" => ["en-GB", :en] }]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
