8c9d3496737b8b0b159e0fe8655d64bab63be8b3
Nikolai B <nikolai-b@users.noreply.github.com>
2022-06-17 16:15:01 +0100

    Fix deprecation
    MailDeliveryJob is the new name but use the old class for now

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ Bundler.require(*Rails.groups)
 module Cyclescape
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.action_mailer.delivery_job = "ActionMailer::DeliveryJob"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
