a2b707fd5000ed44129506ee072e6a59293e3463
Nikolai B <nikolai-b@users.noreply.github.com>
2022-07-10 21:44:13 +0100

    Fix message links for lazy loading
    Fixes #1017

--- app/assets/javascripts/thread_scroller.js
+++ app/assets/javascripts/thread_scroller.js
@@ -1,33 +1,60 @@
 $(document).ready(function () {
   var lastViewedMessage
-  var lastViewedMessageId = $('[data-view-message-id]').data('view-message-id')
-  if (lastViewedMessageId) {
-    lastViewedMessage = document.getElementById('message_' + lastViewedMessageId)
+  var lastViewedMessageID = $('[data-view-message-id]').data('view-message-id')
+  var messageWantedID = window.location.hash.substr(1)
+  var messageWanted = messageWantedID && messageWantedID.indexOf("message_") === 0
+  var messageWantedEl
+  var getMessages = function() {
+    return $.ajax(
+      {
+        url: window.location.pathname,
+        data: {initiallyLoadedFrom: document.querySelector('[data-initially-loaded-from]').dataset.initiallyLoadedFrom},
+        dataType: 'script'
+      }
+    )
+  }
+  if (lastViewedMessageID) {
+    lastViewedMessage = document.getElementById('message_' + lastViewedMessageID)
     lastViewedMessage && lastViewedMessage.scrollIntoView()
   }
-  var initiallyLoadedFrom = document.querySelector('[data-initially-loaded-from]')
-  if (initiallyLoadedFrom) {
-    var options = {
-      rootMargin: '1500px 0px 0px 0px',
-      threshold: 0
-    }
-    var obs = new IntersectionObserver(function (entries) {
-      if (entries[0] && entries[0].isIntersecting) {
-        obs.unobserve(initiallyLoadedFrom)
-        $.ajax(
-          {
-            url: window.location.pathname,
-            data: {initiallyLoadedFrom: document.querySelector('[data-initially-loaded-from]').dataset.initiallyLoadedFrom},
-            dataType: 'script'
-          }
-        ).then(function() {
-          initiallyLoadedFrom = document.querySelector('[data-initially-loaded-from]')
-          if (initiallyLoadedFrom) {
-            obs.observe(initiallyLoadedFrom)
-          }
-        })
+
+  var loadMoreObserver = function() {
+    var initiallyLoadedFrom = document.querySelector('[data-initially-loaded-from]')
+    if (initiallyLoadedFrom) {
+      var options = {
+        rootMargin: '1500px 0px 0px 0px',
+        threshold: 0
       }
-    }, options)
-    obs.observe(initiallyLoadedFrom)
+      var obs = new IntersectionObserver(function (entries) {
+        if (entries[0] && entries[0].isIntersecting) {
+          obs.unobserve(initiallyLoadedFrom)
+          getMessages().then(function() {
+            initiallyLoadedFrom = document.querySelector('[data-initially-loaded-from]')
+            if (initiallyLoadedFrom) {
+              obs.observe(initiallyLoadedFrom)
+            }
+          })
+        }
+      }, options)
+      obs.observe(initiallyLoadedFrom)
+    }
+  }
+
+  if(messageWanted){
+    var scrollToMessageWanted = function() {
+      messageWantedEl = document.getElementById(messageWantedID)
+      if(messageWantedEl) {
+        messageWantedEl.scrollIntoView()
+        loadMoreObserver()
+        return
+      }
+      if (!document.querySelector('[data-initially-loaded-from]')) {
+        return
+      }
+      getMessages().then(scrollToMessageWanted)
+    }
+    scrollToMessageWanted()
+  } else {
+    loadMoreObserver()
   }
 })
