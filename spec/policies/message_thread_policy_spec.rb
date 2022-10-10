80a47bdcd1dadfb68b0fd539f1a43574e033e4a4
Nikolai B <nikolai-b@users.noreply.github.com>
2022-10-10 11:56:56 +0100

    wip

--- spec/policies/message_thread_policy_spec.rb
+++ spec/policies/message_thread_policy_spec.rb
@@ -102,4 +102,56 @@ RSpec.describe MessageThreadPolicy do
       end
     end
   end
+
+  context "with a non logged in user" do
+    let(:thread) { create :message_thread }
+    let(:user) { nil }
+    let(:allowed_actions) { %i[show] }
+    let(:forbiden_actions) { %i[new create edit update destroy close vote_detail] }
+
+    it do
+      expect(subject).to permit_actions(allowed_actions)
+      expect(subject).to forbid_actions(forbiden_actions)
+    end
+  end
+
+  context "private thread (direct messages)" do
+    let(:user) { creating_user }
+    let(:thread) { create :message_thread, created_by: creating_user, user: recieving_user, privacy: MessageThread::PRIVATE  }
+
+    context "both users have public profiles" do
+      let(:creating_user) { create :user }
+      let(:recieving_user) { create :user }
+      let(:allowed_actions) { %i[new create show edit update close vote_detail] }
+      let(:forbiden_actions) { %i[open destroy] }
+
+      it do
+        expect(subject).to permit_actions(allowed_actions)
+      end
+      context "same with recieving_user" do
+        let(:user) { recieving_user }
+        let(:allowed_actions) { %i[new create show close vote_detail] }
+        let(:forbiden_actions) { %i[open destroy edit update] }
+
+        it do
+          expect(subject).to permit_actions(allowed_actions)
+        end
+      end
+    end
+
+    context "have private profile but share a group" do
+    end
+
+    context "have private profile and requesting to be in a group other is committee of" do
+    end
+
+    context "have private profile and do not share a group" do
+    end
+
+    context "creating user has blocked other user" do
+    end
+
+    context "other user has blocked creating user" do
+    end
+  end
 end
