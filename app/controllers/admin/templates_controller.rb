90cc8276f1d40d4c2a3b53e93d97c3fcd7a31429
Nikolai B <nikolai-b@users.noreply.github.com>
2024-07-28 22:24:45 +0100

    Refactor admin controllers from compact style

--- app/controllers/admin/templates_controller.rb
+++ app/controllers/admin/templates_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-class Admin::TemplatesController < ApplicationController
-  def show
-    template_name = "admin/templates/" + File.basename(params[:template].to_s)
-    if template_exists? template_name
-      render template: template_name
-    else
-      raise ActionController::RoutingError, "Not Found"
+module Admin
+  class TemplatesController < BaseController
+    def show
+      template_name = "admin/templates/" + File.basename(params[:template].to_s)
+      if template_exists? template_name
+        render template: template_name
+      else
+        raise ActionController::RoutingError, "Not Found"
+      end
     end
   end
 end
