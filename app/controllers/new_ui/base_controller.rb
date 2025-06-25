53d02edd1d687cde46eb22514159f5554d31a270
Nikolai B <nikolai-b@users.noreply.github.com>
2025-06-25 14:46:11 +0100

    Rm old attempt at new UI

--- app/controllers/new_ui/base_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module NewUi
-  class BaseController < ApplicationController
-    layout -> (controller) { controller.request.xhr? ? false : 'application' }
-  end
-end
